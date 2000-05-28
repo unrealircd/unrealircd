b2e402231d8f06859bb7b8ab30e9945f2f858b38
cmunk <devnull@localhost>
2000-05-28 08:45:31 +0000

    Addition of Unreal3.1-beta3

---
+++ include/badwords.h
@@ -0,0 +1,20 @@
+#ifndef __BADWORDS_H
+#define __BADWORDS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <regex.h>
+
+#define MAX_MATCH       1
+#define MAX_WORDLEN	64
+#define MAX_WORDS	30
+
+#define PATTERN		"\\w*%s\\w*"
+#define REPLACEWORD	"<censored>"
+
+char 	*stripbadwords(char *, int);
+int	 loadbadwords(char *, int);
+void	 freebadwords(void);
+
+#endif
