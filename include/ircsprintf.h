da53d2f0bf638dc928439afcf86b21dad926ce8d
drbin <devnull@localhost>
2000-06-16 20:08:11 +0000

    Added DEBUGMODE support in ircsprintf.h

--- include/ircsprintf.h
+++ include/ircsprintf.h
@@ -14,8 +14,17 @@
  * Proto types
  */
 
+/* You do want it to work in debug mode yes ? --DrBin */
+
+#ifndef DEBUGMODE
 extern char *ircvsprintf(char *str, const char *format, va_list);
-extern char *ircsprintf(char *str, const char *format, ...); 
+extern char *ircsprintf(char *str, const char *format, ...);
+#else
+#define ircsprintf sprintf
+#define ircvsprintf vsprintf
+#endif
+
+
 extern const char atoi_tab[4000];
 
 #endif
