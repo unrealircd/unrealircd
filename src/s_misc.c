cd690443668cdb4c6013e267255b704033297b22
stskeeps <devnull@localhost>
2000-12-01 21:10:13 +0000

    .

--- src/s_misc.c
+++ src/s_misc.c
@@ -736,13 +736,12 @@ static void exit_one_client_backend(cptr, sptr, from, comment, split)
 	}
 
 	/* Remove sptr from the client list */
-	if (IsServer(sptr) || IsClient(sptr))
-		if (del_from_client_hash_table(sptr->name, sptr) != 1)
-			Debug((DEBUG_ERROR, "%#x !in tab %s[%s] %#x %#x %#x %d %d %#x",
-			    sptr, sptr->name,
-			    sptr->from ? sptr->from->sockhost : "??host",
-			    sptr->from, sptr->next, sptr->prev, sptr->fd,
-			    sptr->status, sptr->user));
+	if (del_from_client_hash_table(sptr->name, sptr) != 1)
+		Debug((DEBUG_ERROR, "%#x !in tab %s[%s] %#x %#x %#x %d %d %#x",
+		    sptr, sptr->name,
+		    sptr->from ? sptr->from->sockhost : "??host",
+		    sptr->from, sptr->next, sptr->prev, sptr->fd,
+		    sptr->status, sptr->user));
 	if (IsRegisteredUser(sptr))
 		hash_check_notify(sptr, RPL_LOGOFF);
 	remove_client_from_list(sptr);
