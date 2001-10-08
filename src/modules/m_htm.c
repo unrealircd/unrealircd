6bad7c8dfa2d45f766f51b339f11376d73c94f72
codemastr <devnull@localhost>
2001-10-08 17:06:32 +0000

    Added headers to all the new modules

--- src/modules/m_htm.c
+++ src/modules/m_htm.c
@@ -1,8 +1,23 @@
 /*
- * Module skeleton, by Carsten V. Munk 2001 <stskeeps@tspre.org>
- * May be used, modified, or changed by anyone, no license applies.
- * You may relicense this, to any license
+ *   Unreal Internet Relay Chat Daemon, src/modules/m_htm.c
+ *   (C) 2000-2001 Carsten V. Munk and the UnrealIRCd Team
+ *   Moved to modules by Fish (Justin Hammond)
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+
 #include "config.h"
 #include "struct.h"
 #include "common.h"
