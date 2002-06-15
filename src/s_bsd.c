b5ae2f7fc8c5483f526a768ded15a8f9e2b9ed1e
stskeeps <devnull@localhost>
2002-06-15 20:12:13 +0000

    crack

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -207,7 +207,7 @@ void close_connections(void)
   OpenFiles = 0;
   LastSlot = -1;
 #ifdef _WIN32
-  WSACleanup();
+	  WSACleanup();
 #endif
 }
 
@@ -1541,6 +1541,7 @@ int  read_message(time_t delay)
 int  read_message(time_t delay, fdlist *listp)
 #endif
 {
+#define my_FD_SET(x,y) do { if (fcntl(x, F_GETFD, &sockerr) == -1) abort(); FD_SET(x,y); } while(0)
 	aClient *cptr;
 	int  nfds;
 	struct timeval wait;
@@ -1591,12 +1592,12 @@ int  read_message(time_t delay, fdlist *listp)
 				Debug((DEBUG_NOTICE, "auth on %x %d", cptr, i));
 				if (cptr->authfd >= 0)
 				{
-					FD_SET(cptr->authfd, &read_set);
+					my_FD_SET(cptr->authfd, &read_set);
 #ifdef _WIN32
-					FD_SET(cptr->authfd, &excpt_set);
+					my_FD_SET(cptr->authfd, &excpt_set);
 #endif
 					if (cptr->flags & FLAGS_WRAUTH)
-						FD_SET(cptr->authfd, &write_set);
+						my_FD_SET(cptr->authfd, &write_set);
 				}
 			}
 			if (DoingDNS(cptr) || DoingAuth(cptr)
@@ -1605,30 +1606,28 @@ int  read_message(time_t delay, fdlist *listp)
 			if (IsMe(cptr) && IsListening(cptr))
 			{
 				if (cptr->fd >= 0)
-					FD_SET(cptr->fd, &read_set);
+					my_FD_SET(cptr->fd, &read_set);
 			}
 			else if (!IsMe(cptr))
 			{
 				if (DBufLength(&cptr->recvQ) && delay2 > 2)
 					delay2 = 1;
 				if ((cptr->fd >= 0) && (DBufLength(&cptr->recvQ) < 4088))
-					FD_SET(cptr->fd, &read_set);
+					my_FD_SET(cptr->fd, &read_set);
 			}
 			if ((cptr->fd >= 0) && (DBufLength(&cptr->sendQ) || IsConnecting(cptr) ||
 			    (DoList(cptr) && IsSendable(cptr))))
 			{
-				if (cptr->fd <= 0)
-					abort();
-				FD_SET(cptr->fd, &write_set);
+				my_FD_SET(cptr->fd, &write_set);
 			}
 		}
 
 #ifndef _WIN32
 		if (resfd >= 0)
-			FD_SET(resfd, &read_set);
+			my_FD_SET(resfd, &read_set);
 #endif
 		if (me.fd >= 0)
-			FD_SET(me.fd, &read_set);
+			my_FD_SET(me.fd, &read_set);
 
 		wait.tv_sec = MIN(delay, delay2);
 		wait.tv_usec = 0;
