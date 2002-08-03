c2b48d3ce40a1937ace05241d0d12135bae197fb
stskeeps <devnull@localhost>
2002-08-03 19:01:26 +0000

    - Fixed SSL compile error wrt to /trace

--- src/s_serv.c
+++ src/s_serv.c
@@ -3706,12 +3706,12 @@ CMD_FUNC(m_trace)
 			  break;
 #ifdef USE_SSL
 		  case STAT_SSL_CONNECT_HANDSHAKE:
-		  	sendto_one(sptr, rpl_str(RPL_NEWTYPE), me.name,
+		  	sendto_one(sptr, rpl_str(RPL_TRACENEWTYPE), me.name,
 		  	 parv[0], "SSL-Connect-Handshake", name); 
 			cnt++;
 			break;
 		  case STAT_SSL_ACCEPT_HANDSHAKE:
-		  	sendto_one(sptr, rpl_str(RPL_NEWTYPE), me.name,
+		  	sendto_one(sptr, rpl_str(RPL_TRACENEWTYPE), me.name,
 		  	 parv[0], "SSL-Accept-Handshake", name); 
 			cnt++;
 			break;
