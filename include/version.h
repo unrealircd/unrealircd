2182c90be5edf3760e77bbda24c9a64be3160ae2
codemastr <devnull@localhost>
2002-12-01 18:23:11 +0000

    Release preparations

--- include/version.h
+++ include/version.h
@@ -30,7 +30,7 @@
 #define PATCH1  		"3"
 #define PATCH2  		".2"
 #define PATCH3  		"-Selene"
-#define PATCH4  		"[beta12]"
+#define PATCH4  		"[beta13]"
 #define PATCH5  		""
 #define PATCH6  		""
 #define PATCH7  		""
