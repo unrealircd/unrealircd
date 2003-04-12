f4733143120c4f0ea1ba0132c8e3a26cd5c482bc
Bram Matthys <syzop@vulnscan.org>
2003-04-12 20:21:38 +0000

    - Improved IPv6 throttle hashing

--- src/hash.c
+++ src/hash.c
@@ -749,14 +749,14 @@ int	hash_throttling(struct IN_ADDR *in)
 {
 #ifdef INET6
 	u_char *cp;
+	unsigned int alpha, beta;
 #endif
 #ifndef INET6
 	return ((unsigned int)in->s_addr % THROTTLING_HASH_SIZE); 
 #else
-	cp = (u_char *) &in->s6_addr;
-	return (cp[0] ^ cp[1] ^ cp[2] ^ cp[3] ^ cp[4] ^ cp[5] ^
-		cp[6] ^ cp[7] ^ cp[8] ^ cp[9] ^ cp[10] ^ cp[11] ^ 
-		cp[12] ^ cp[13] ^ cp[14] ^ cp[15]);
+	memcpy(&alpha, &cp[8], sizeof(alpha));
+	memcpy(&beta, &cp[12], sizeof(beta));
+	return ((alpha ^ beta) % THROTTLING_HASH_SIZE);
 #endif
 }
 
