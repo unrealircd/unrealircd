c2a4509d9cb30e62b3727dcf426fca8fd53e38cf
codemastr <devnull@localhost>
2003-04-13 23:46:04 +0000

    Made the channel be hidden from /who if you are +p

--- src/modules/m_who.c
+++ src/modules/m_who.c
@@ -772,6 +772,8 @@ static char *first_visible_channel(aClient *sptr, aClient *acptr, int *flg)
       aChannel *chptr = lp->chptr;
       int cansee = ShowChannel(sptr, chptr);
 
+      if (cansee && (acptr->umodes & UMODE_HIDEWHOIS) && !IsMember(sptr, chptr))
+		cansee = 0;
       if (!cansee)
 	{
 	  if (IsAnOper(sptr))
