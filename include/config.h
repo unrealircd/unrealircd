f2ee326b4cfe788b7891d2abafbc76e60fc54ffb
codemastr <devnull@localhost>
2003-08-04 20:54:51 +0000

    ...

--- include/config.h
+++ include/config.h
@@ -549,7 +549,7 @@
  * 32 possible modes which can also be used in modules.
  * This is currently considered experimental however.
  */
-#define EXTCMODE
+#undef EXTCMODE
 
 /* ------------------------- END CONFIGURATION SECTION -------------------- */
 #define MOTD MPATH
