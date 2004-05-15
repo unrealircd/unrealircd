e1cc540182d6a71327495fa3143b64290a0d275f
codemastr <devnull@localhost>
2004-05-15 19:42:24 +0000

    Upgraded to TRE 0.6.7

--- include/win32/regex.h
+++ include/win32/regex.h
@@ -1,7 +1,7 @@
 /*
   regex.h - POSIX.2 compatible regexp interface and TRE extensions
 
-  Copyright (C) 2001-2003 Ville Laurikari <vl@iki.fi>.
+  Copyright (C) 2001-2004 Ville Laurikari <vl@iki.fi>.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License version 2 (June
@@ -26,6 +26,10 @@
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif /* HAVE_SYS_TYPES_H */
+
+#ifdef HAVE_LIBUTF8_H
+#include <libutf8.h>
+#endif /* HAVE_LIBUTF8_H */
 
 #ifdef TRE_USE_SYSTEM_REGEX_H
 /* Include the system regex.h to make TRE ABI compatible with the
@@ -182,6 +186,19 @@ int regawnexec(const regex_t *preg, const wchar_t *string, size_t len,
 /* Sets the parameters to default values. */
 void regaparams_default(regaparams_t *params);
 #endif /* TRE_APPROX */
+
+/* Returns the version string.  The returned string is static. */
+char *tre_version(void);
+
+int tre_config(int query, void *result);
+
+enum {
+  TRE_CONFIG_APPROX,
+  TRE_CONFIG_WCHAR,
+  TRE_CONFIG_MULTIBYTE,
+  TRE_CONFIG_SYSTEM_ABI,
+  TRE_CONFIG_VERSION
+};
 
 #ifdef __cplusplus
 }
