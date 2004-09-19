47c5c36bda1d39a771173710a7e77158296c81cb
Bram Matthys <syzop@vulnscan.org>
2004-09-19 16:13:03 +0000

    - Added salted passwords. Salts are useful because it "protects" against stored-plaintext   attacks (eg: rainbow) and prevents cracking of several passwords at once.   This change means /MKPASSWD will now just generate a different string than before.   Do note however, that the old syntax/encrypted passwords will still work and _will continue   to work_ in the future, for at least the whole 3.2* series.   If you are concerned with security and have some time, then converting your passwords   is probably a good idea... Just in case your configuration file gets stolen one day ;). - MD5 password encryption is now always available on *NIX, even if SSL is disabled.

--- include/config.h
+++ include/config.h
@@ -93,14 +93,6 @@
 #undef SECURECHANMSGSONLYGOTOSECURE
 
 /*
- * be compatible with older cloak keys? If you link to servers beta4 and 
- * earlier without this the cloak keys will produce diff results
- * Not recommended, however, as beta4 and earlier 3.2 has an insecure
- * cloak algo -griever
- */
-#undef COMPAT_BETA4_KEYS
-
-/*
   If you want SHUN_NOTICES, define this
 */
 #undef SHUN_NOTICES
