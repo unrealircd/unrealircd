50520aee846d71ca4c61ad1cfa7a421543f49f94
Bram Matthys <syzop@vulnscan.org>
2005-02-12 23:33:26 +0000

    - Added a feature to +b ~c, ~c:[prefix]<#channel>, prefix can be +/%/@/&/~ and will   check if the user is voiced/halfoped/etc.. Especially useful for +e ~c. Idea from   Bugz (#0002198). Obviously all servers need to be upgraded to make this work.

--- src/extbans.c
+++ src/extbans.c
@@ -155,12 +155,17 @@ char tmpbuf[512];
 char *extban_modec_conv_param(char *para)
 {
 static char retbuf[CHANNELLEN+6];
-char *chan, *p;
+char *chan, *p, symbol='\0';
 
 	strncpyzt(retbuf, para, sizeof(retbuf));
 	chan = retbuf+3;
+
+	if ((*chan == '+') || (*chan == '%') || (*chan == '%') ||
+	    (*chan == '@') || (*chan == '&') || (*chan == '~'))
+	    chan++;
 	if (*chan != '#')
 		return NULL;
+
 	if (strlen(chan) > CHANNELLEN)
 		chan[CHANNELLEN] = '\0';
 	clean_channelname(chan);
@@ -170,15 +175,47 @@ char *chan, *p;
 	/* on a sidenote '#' is allowed because it's a valid channel (atm) */
 	return retbuf;
 }
+static int extban_modec_compareflags(char symbol, int flags)
+{
+int require=0;
+
+	if (symbol == '+')
+		require = CHFL_VOICE|CHFL_HALFOP|CHFL_CHANOP|CHFL_CHANPROT|CHFL_CHANOWNER;
+	else if (symbol == '%')
+		require = CHFL_HALFOP|CHFL_CHANOP|CHFL_CHANPROT|CHFL_CHANOWNER;
+	else if (symbol == '@')
+		require = CHFL_CHANOP|CHFL_CHANPROT|CHFL_CHANOWNER;
+	else if (symbol == '&')
+		require = CHFL_CHANPROT|CHFL_CHANOWNER;
+	else if (symbol == '~')
+		require = CHFL_CHANOWNER;
+
+	if (flags & require)
+		return 1;
+	return 0;
+}
 int extban_modec_is_banned(aClient *sptr, aChannel *chptr, char *ban, int type)
 {
 Membership *lp;
-char *p = ban+3;
+char *p = ban+3, symbol = '\0';
 
+	if (*p != '#')
+	{
+		symbol = *p;
+		p++;
+	}
 	for (lp = sptr->user->channel; lp; lp = lp->next)
 	{
 		if (!strcasecmp(lp->chptr->chname, p))
-			return 1;
+		{
+			/* Channel matched, check symbol if needed (+/%/@/etc) */
+			if (symbol)
+			{
+				if (extban_modec_compareflags(symbol, lp->flags))
+					return 1;
+			} else
+				return 1;
+		}
 	}
 	return 0;
 }
