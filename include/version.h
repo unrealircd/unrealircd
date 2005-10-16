8b741865c3ee2e4f991ff00c5da03831cc048779
Bram Matthys <syzop@vulnscan.org>
2005-10-16 14:40:26 +0000

    - Added 4 UNREAL_VERSION_* macro's that can be useful for 3rd party modules to find out the   unreal version that the user is using. I presume this can be helpful (although nobody ever   suggested it ;p). The macros (#define's) are:   UNREAL_VERSION_GENERATION    The generation version number        eg: 3 for 3.2.4   UNREAL_VERSION_MAJOR         The major version number             eg: 2 for 3.2.4   UNREAL_VERSION_MINOR         The minor version number             eg: 4 for 3.2.4                                This can be negative for unstable,                                alpha and beta versions.   UNREAL_VERSION_TIME          Year + week of the day (starting     eg: 200541                                on Monday), this is updated on                                the CVS server every week.   The first 3 are for nicely identifiying the version, the 4th can be useful in case   you want to support CVS and/or want some more control.

--- include/version.h
+++ include/version.h
@@ -23,9 +23,31 @@
 #endif
      /**/
 #define COMPILEINFO DEBUGMODESET DEBUGSET
-/*
- * Version Unreal3.2.2
+
+/* Version info follows, current: Unreal3.2.3
+ * Please be sure to update ALL fields when changing the version.
+ * Also don't forget to bump the protocol version every release.
  */
+
+/** These UNREAL_VERSION_* macros can be used so (3rd party) modules
+ * can easily distinguish versions.
+ */
+
+/** The generation version number (eg: 3 for Unreal3*) */
+#define UNREAL_VERSION_GENERATION   3
+
+/** The major version number (eg: 2 for Unreal3.2*) */
+#define UNREAL_VERSION_MAJOR        2
+
+/** The minor version number (eg: 1 for Unreal3.2.1), negative numbers for unstable/alpha/beta */
+#define UNREAL_VERSION_MINOR        3
+
+/** Year + week of the year (with Monday as first day of the week).
+ * Can be useful if the above 3 versionids are insufficient for you (eg: you want to support CVS).
+ * This is updated automatically on the CVS server every Monday. so don't touch it.
+ */
+#define UNREAL_VERSION_TIME         200541
+
 #define UnrealProtocol 		2306
 #define PATCH1  		"3"
 #define PATCH2  		".2"
