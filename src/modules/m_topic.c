eee6d3c8d1e5be428fff6a08e6c721845c9a7b75
Bram Matthys <syzop@vulnscan.org>
2006-04-09 18:37:38 +0000

    - Local opers may now use /TRACE (local only), suggested by GSF19 (#0002365).

--- src/modules/m_topic.c
+++ src/modules/m_topic.c
@@ -94,11 +94,13 @@ DLLFUNC int MOD_UNLOAD(m_topic)(int module_unload)
 */
 DLLFUNC CMD_FUNC(m_topic)
 {
-	aChannel *chptr = NullChn;
-	char *topic = NULL, *name, *tnick = NULL;
-	TS   ttime = 0;
-	int  topiClen = 0;
-	int  nicKlen = 0;
+aChannel *chptr = NullChn;
+char *topic = NULL, *name, *tnick = NULL;
+TS   ttime = 0;
+int  topiClen = 0;
+int  nicKlen = 0;
+int ismember; /* cache: IsMember() */
+long flags = 0; /* cache: membership flags */
 
 	if (parc < 2)
 	{
@@ -123,9 +125,14 @@ DLLFUNC CMD_FUNC(m_topic)
 			    me.name, parv[0], name);
 			return 0;
 		}
+		
+		ismember = IsMember(sptr, chptr); /* CACHE */
+		if (ismember)
+			flags = get_access(sptr, chptr); /* CACHE */
+		
 		if (parc > 2 || SecretChannel(chptr))
 		{
-			if (!IsMember(sptr, chptr) && !IsServer(sptr)
+			if (!ismember && !IsServer(sptr)
 			    && !IsOper(sptr) && !IsULine(sptr))
 			{
 				sendto_one(sptr, err_str(ERR_NOTONCHANNEL),
@@ -145,9 +152,9 @@ DLLFUNC CMD_FUNC(m_topic)
 
 		if (!topic)	/* only asking  for topic  */
 		{
-			if ((chptr->mode.mode & MODE_OPERONLY && !IsAnOper(sptr) && !IsMember(sptr, chptr)) ||
-			    (chptr->mode.mode & MODE_ADMONLY && !IsAdmin(sptr) && !IsMember(sptr, chptr)) ||
-			    (is_banned(sptr,chptr,BANCHK_JOIN) && !IsAnOper(sptr) && !IsMember(sptr, chptr))) {
+			if ((chptr->mode.mode & MODE_OPERONLY && !IsAnOper(sptr) && !ismember) ||
+			    (chptr->mode.mode & MODE_ADMONLY && !IsAdmin(sptr) && !ismember) ||
+			    (is_banned(sptr,chptr,BANCHK_JOIN) && !IsAnOper(sptr) && !ismember)) {
 				sendto_one(sptr, err_str(ERR_NOTONCHANNEL), me.name, parv[0], name);
 				return 0;
 			}
@@ -248,7 +255,16 @@ DLLFUNC CMD_FUNC(m_topic)
 				ircsprintf(buf, "You cannot change the topic on %s while being banned", chptr->chname);
 				sendto_one(sptr, err_str(ERR_CANNOTDOCOMMAND), me.name, parv[0], "TOPIC",  buf);
 				return -1;
+			} else
+			if (MyClient(sptr) && (flags == 0) && (chptr->mode.mode & MODE_MODERATED))
+			{
+				char buf[512];
+				/* With +m and -t, only voice and higher may change the topic */
+				ircsprintf(buf, "You need at least voice (+v) in order to change the topic on %s (channel is +m)", chptr->chname);
+				sendto_one(sptr, err_str(ERR_CANNOTDOCOMMAND), me.name, parv[0], "TOPIC",  buf);
+				return -1;
 			}
+			
 			/* ready to set... */
 			if (MyClient(sptr))
 			{
