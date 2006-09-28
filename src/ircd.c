9aed223b17280073d065eee2f5af0ad60a4f8382
Bram Matthys <syzop@vulnscan.org>
2006-09-28 20:43:52 +0000

    - Fixed zlib version check: 1.x is compatible with all 1.*, etc. (#0002966).

--- src/ircd.c
+++ src/ircd.c
@@ -845,7 +845,7 @@ int error = 0;
 #ifdef ZIP_LINKS
 	runtime = zlibVersion();
 	compiledfor = ZLIB_VERSION;
-	if (strcasecmp(compiledfor, runtime))
+	if (*compiledfor != *runtime)
 	{
 		version_check_logerror("Zlib version mismatch: compiled for '%s', library is '%s'",
 			compiledfor, runtime);
