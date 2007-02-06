8c6bb314c5d3d06c0c0d4e9f4c8fb6dfb2db6104
Bram Matthys <syzop@vulnscan.org>
2007-02-06 15:33:12 +0000

    - Changed password length from 32 to 48, is allocated dynamically anyway.

--- include/struct.h
+++ include/struct.h
@@ -172,7 +172,7 @@ typedef unsigned int u_int32_t;	/* XXX Hope this works! */
 #define	REALLEN	 	50
 #define	TOPICLEN	307
 #define	CHANNELLEN	32
-#define	PASSWDLEN 	32	/* orig. 20, changed to 32 for nickpasswords */
+#define	PASSWDLEN 	48	/* was 20, then 32, now 48. */
 #define	KEYLEN		23
 #define LINKLEN		32
 #define	BUFSIZE		512	/* WARNING: *DONT* CHANGE THIS!!!! */
