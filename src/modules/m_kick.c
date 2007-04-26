5545cc49b9d6e53c786561dd4ac8c2330937d69c
stskeeps <devnull@localhost>
2007-04-26 15:18:40 +0000

    ? Makefile ? Unreal.patch ? bindir.patch ? cert-swhois.patch.txt ? config.log ? config.settings ? config.status ? config_sslzipdirfix.patch ? dgets.patch ? diff-msg-wildcardfix ? diff-remove-addline ? diff-remove-unused-crap ? ircd.log ? ircd.pid ? ircd.tune ? locop.patch ? locops_see_ulines_in_map.patch ? login_page.php?return=%2Ffile_download.php?file_id=408 ? myoper.patch ? pingseconds.patch ? quitreaderrormsg.patch ? readerrorquitmsg.patch ? s_conf.patch ? s_serv.patch ? server.cert.pem ? server.key.pem ? server.req.pem ? ssl.rnd ? tmp ? unreal ? unreal318fix.patch ? unrealircd.conf ? userip.patch ? extras/c-ares ? extras/c-ares-1.3.2 ? extras/c-ares.tar ? extras/regexp ? extras/tre-0.7.2 ? extras/tre.tar ? include/setup.h ? ircdcron/ircdchk ? src/ircd ? src/version.c ? src/modules/Makefile ? src/modules/m_cycle.cpp ? src/modules/test.cpp ? src/modules/chanmodes/Makefile Index: Changes =================================================================== RCS file: /home/cmunk/ircsystems/cvsroot/unreal/Changes,v retrieving revision 1.1.1.1.2.1.2.1.2.2380 diff -u -r1.1.1.1.2.1.2.1.2.2380 Changes --- Changes	26 Apr 2007 12:55:16 -0000	1.1.1.1.2.1.2.1.2.2380 +++ Changes	26 Apr 2007 15:18:34 -0000 @@ -1632,3 +1632,6 @@    Devel.WolfSage.org[192.168.2.97] closed the connection. This would    probably have caused netsplits to go unnoticed.  - #0002095 removing /helpop broadcasting +- #0003298 reported by Stealth patched by WolfSage, regarding KICK now sends +  proper channel name to other servers, Local KICK now displays proper +  channel name from other servers Index: src/modules/m_kick.c =================================================================== RCS file: /home/cmunk/ircsystems/cvsroot/unreal/src/modules/Attic/m_kick.c,v retrieving revision 1.1.2.6 diff -u -r1.1.2.6 m_kick.c --- src/modules/m_kick.c	25 Apr 2007 17:37:05 -0000	1.1.2.6 +++ src/modules/m_kick.c	26 Apr 2007 15:18:35 -0000 @@ -149,10 +149,10 @@  					goto attack;
     				/* Note for coders regarding oper override:
    -				 * always let a remote kick (=from a user on another server) trough or
    +				 * always let a remote kick (=from a user on another server) through or
     				 * else we will get desynched. In short this means all the denying should
     				 * always contain a && MyClient(sptr) [or sptr!=cptr] and at the end
    -				 * a remote kick should always be allowed (pass trough). -- Syzop
    +				 * a remote kick should always be allowed (pass through). -- Syzop
     				 */
    
     				/* applies to everyone (well except remote/ulines :p) */
    @@ -336,12 +336,12 @@
     						/* NORMAL */
     						sendto_channel_butserv(chptr,
     						    sptr, ":%s KICK %s %s :%s",
    -						    parv[0], name, who->name, comment);
    +						    parv[0], chptr->chname, who->name, comment);
     					}
     				}
     				sendto_serv_butone_token(cptr, parv[0],
     				    MSG_KICK, TOK_KICK, "%s %s :%s",
    -				    name, who->name, comment);
    +				    chptr->chname, who->name, comment);
     				if (lp)
     				{
     					remove_user_from_channel(who, chptr);

--- src/modules/m_kick.c
+++ src/modules/m_kick.c
@@ -149,10 +149,10 @@ CMD_FUNC(m_kick)
 					goto attack;
 
 				/* Note for coders regarding oper override:
-				 * always let a remote kick (=from a user on another server) trough or
+				 * always let a remote kick (=from a user on another server) through or
 				 * else we will get desynched. In short this means all the denying should
 				 * always contain a && MyClient(sptr) [or sptr!=cptr] and at the end
-				 * a remote kick should always be allowed (pass trough). -- Syzop
+				 * a remote kick should always be allowed (pass through). -- Syzop
 				 */
 
 				/* applies to everyone (well except remote/ulines :p) */
@@ -336,12 +336,12 @@ CMD_FUNC(m_kick)
 						/* NORMAL */
 						sendto_channel_butserv(chptr,
 						    sptr, ":%s KICK %s %s :%s",
-						    parv[0], name, who->name, comment);
+						    parv[0], chptr->chname, who->name, comment);
 					}
 				}
 				sendto_serv_butone_token(cptr, parv[0],
 				    MSG_KICK, TOK_KICK, "%s %s :%s",
-				    name, who->name, comment);
+				    chptr->chname, who->name, comment);
 				if (lp)
 				{
 					remove_user_from_channel(who, chptr);
