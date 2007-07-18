2711cd2c86758a182095ece2f70ad3e632bd0eea
stskeeps <devnull@localhost>
2007-07-18 11:25:14 +0000

    - Changed version into 3.2.8 as this will be the target for devel branch

--- include/version.h
+++ include/version.h
@@ -50,9 +50,9 @@
 
 #define UnrealProtocol 		2308
 #define PATCH1  		"3"
-#define PATCH2  		".3"
-#define PATCH3  		"-pre-alpha"
-#define PATCH4  		""
+#define PATCH2  		".2"
+#define PATCH3  		".8"
+#define PATCH4  		"-devel"
 #define PATCH5  		""
 #define PATCH6  		""
 #define PATCH7  		""
