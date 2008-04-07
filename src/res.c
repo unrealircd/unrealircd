99ba7c9acd96dc9faf080a6fe357beb347bc0902
Bram Matthys <syzop@vulnscan.org>
2008-04-07 15:03:31 +0000

    - Upgraded c-ares to 1.5.1, thanks to aegis for the partial patch (#0003671).   This also fixed a curl compile/run issue, reported by static-x (#0003545).

--- src/res.c
+++ src/res.c
@@ -49,9 +49,9 @@
 #include <res.h>
 
 /* Forward declerations */
-void unrealdns_cb_iptoname(void *arg, int status, struct hostent *he);
-void unrealdns_cb_nametoip_verify(void *arg, int status, struct hostent *he);
-void unrealdns_cb_nametoip_link(void *arg, int status, struct hostent *he);
+void unrealdns_cb_iptoname(void *arg, int status, int timeouts, struct hostent *he);
+void unrealdns_cb_nametoip_verify(void *arg, int status, int timeouts, struct hostent *he);
+void unrealdns_cb_nametoip_link(void *arg, int status, int timeouts, struct hostent *he);
 void unrealdns_delasyncconnects(void);
 static unsigned int unrealdns_haship(void *binaryip, int length);
 static void unrealdns_addtocache(char *name, void *binaryip, int length);
@@ -240,7 +240,7 @@ char ipv4[4];
 #endif
 }
 
-void unrealdns_cb_iptoname(void *arg, int status, struct hostent *he)
+void unrealdns_cb_iptoname(void *arg, int status, int timeouts, struct hostent *he)
 {
 DNSReq *r = (DNSReq *)arg;
 DNSReq *newr;
@@ -290,7 +290,7 @@ char *p;
 }
 
 
-void unrealdns_cb_nametoip_verify(void *arg, int status, struct hostent *he)
+void unrealdns_cb_nametoip_verify(void *arg, int status, int timeouts, struct hostent *he)
 {
 DNSReq *r = (DNSReq *)arg;
 aClient *acptr = r->cptr;
@@ -363,7 +363,7 @@ bad:
 	unrealdns_freeandremovereq(r);
 }
 
-void unrealdns_cb_nametoip_link(void *arg, int status, struct hostent *he)
+void unrealdns_cb_nametoip_link(void *arg, int status, int timeouts, struct hostent *he)
 {
 DNSReq *r = (DNSReq *)arg;
 int n;
