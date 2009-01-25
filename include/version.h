09cf3651623a24af25587ccd9766564ea51ad1bb
Bram Matthys <syzop@vulnscan.org>
2009-01-25 11:19:34 +0000

    ** 3.2.8-rc2 release **

--- include/version.h
+++ include/version.h
@@ -52,7 +52,7 @@
 #define PATCH1  		"3"
 #define PATCH2  		".2"
 #define PATCH3  		".8"
-#define PATCH4  		"-rc1"
+#define PATCH4  		"-rc2"
 #define PATCH5  		""
 #define PATCH6  		""
 #define PATCH7  		""
