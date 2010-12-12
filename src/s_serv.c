dcb34f43f68797d56a8aaddf0ad723dade2b99b6
Bram Matthys <syzop@vulnscan.org>
2010-12-12 17:19:43 +0000

    make it compile

--- src/s_serv.c
+++ src/s_serv.c
@@ -274,7 +274,7 @@ char *unrealinfo[] =
 	"* aquanight, WolfSage, ..",
 	"* McSkaf, Zogg, NiQuiL, chasm, llthangel, nighthawk, ..",
 	NULL
-}
+};
 
 void m_info_send(aClient *sptr)
 {
@@ -285,7 +285,7 @@ char **text = unrealinfotext;
 
 	while (*text)
 		sendto_one(sptr, ":%s %d %s :| %s", 
-		    me.name, RPL_INFO, parv[0], *text++);
+		    me.name, RPL_INFO, sptr->name, *text++);
 
 	sendto_one(sptr, ":%s %d %s :|", me.name, RPL_INFO, sptr->name);
 	sendto_one(sptr, ":%s %d %s :|", me.name, RPL_INFO, sptr->name);
