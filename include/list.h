bda05b6876ec3d4dc97fc3c80cda841634523bb7
William Pitcock <nenolod@dereferenced.org>
2012-11-21 23:18:18 +0000

    - Make it possible for nodes which have been delinked to be linked to other lists.

--- include/list.h
+++ include/list.h
@@ -113,9 +113,8 @@ static inline void __list_del_entry(struct list_head *entry)
 
 static inline void list_del(struct list_head *entry)
 {
-	__list_del(entry->prev, entry->next);
-	entry->next = NULL;
-	entry->prev = NULL;
+	__list_del_entry(entry);
+	INIT_LIST_HEAD(entry);
 }
 
 /**
