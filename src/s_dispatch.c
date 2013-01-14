ad25d019b14bd5e8a2665290073118481561bb60
William Pitcock <nenolod@dereferenced.org>
2013-01-14 04:32:39 -0600

    - Enable kqueue support on BSDs.

--- src/s_dispatch.c
+++ src/s_dispatch.c
@@ -223,8 +223,6 @@ void fd_select(time_t delay)
  ***************************************************************************************/
 #ifdef BACKEND_KQUEUE
 
-#warning kqueue support is presently untested.  tell me if it works!
-
 #include <sys/event.h>
 
 static int kqueue_fd = -1;
@@ -238,7 +236,7 @@ void fd_refresh(int fd)
 	if (kqueue_fd == -1)
 		kqueue_fd = kqueue();
 
-	EV_SET(&ev, (uintptr_t) fd, (short) EVFILT_READ, fde->read_callback != NULL ? EV_ADD : EV_DELETE, 0, 0, NULL);
+	EV_SET(&ev, (uintptr_t) fd, (short) EVFILT_READ, fde->read_callback != NULL ? EV_ADD : EV_DELETE, 0, 0, fde);
 	if (kevent(kqueue_fd, &ev, 1, NULL, 0, &(const struct timespec){ .tv_sec = 0, .tv_nsec = 0}) != 0)
 	{
 		if (ERRNO == P_EWOULDBLOCK || ERRNO == P_EAGAIN)
@@ -248,7 +246,7 @@ void fd_refresh(int fd)
 		return;
 	}
 
-	EV_SET(&ev, (uintptr_t) fd, (short) EVFILT_WRITE, fde->write_callback != NULL ? EV_ADD : EV_DELETE, 0, 0, NULL);
+	EV_SET(&ev, (uintptr_t) fd, (short) EVFILT_WRITE, fde->write_callback != NULL ? EV_ADD : EV_DELETE, 0, 0, fde);
 	if (kevent(kqueue_fd, &ev, 1, NULL, 0, &(const struct timespec){ .tv_sec = 0, .tv_nsec = 0}) != 0)
 	{
 		if (ERRNO == P_EWOULDBLOCK || ERRNO == P_EAGAIN)
@@ -284,6 +282,7 @@ void fd_select(time_t delay)
 		ke = &kqueue_events[p];
 		fd = ke->ident;
 		revents = ke->filter;
+		fde = ke->udata;
 
 		if (revents == EVFILT_READ)
 		{
