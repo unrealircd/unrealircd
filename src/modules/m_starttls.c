b3208f2332425182b80eb8ac0a498b39d0d2a795
William Pitcock <nenolod@dereferenced.org>
2013-05-05 17:02:40 +0000

    - Fix STARTTLS replies when the user is unregistered. (#4184)

--- src/modules/m_starttls.c
+++ src/modules/m_starttls.c
@@ -92,11 +92,11 @@ DLLFUNC CMD_FUNC(m_starttls)
 	}
 	if (IsSecure(sptr))
 	{
-		sendto_one(sptr, err_str(ERR_STARTTLS), me.name, sptr->name, "STARTTLS failed. Already using TLS.");
+		sendto_one(sptr, err_str(ERR_STARTTLS), me.name, !BadPtr(sptr->name) ? sptr->name : "*", "STARTTLS failed. Already using TLS.");
 		return 0;
 	}
 	dbuf_delete(&sptr->recvQ, 1000000); /* Clear up any remaining plaintext commands */
-	sendto_one(sptr, rpl_str(RPL_STARTTLS), me.name, sptr->name);
+	sendto_one(sptr, rpl_str(RPL_STARTTLS), me.name, !BadPtr(sptr->name) ? sptr->name : "*");
 	send_queued(sptr);
 
 	SetSSLStartTLSHandshake(sptr);
@@ -118,7 +118,7 @@ DLLFUNC CMD_FUNC(m_starttls)
 	return 0;
 fail:
 	/* Failure */
-	sendto_one(sptr, err_str(ERR_STARTTLS), me.name, sptr->name, "STARTTLS failed");
+	sendto_one(sptr, err_str(ERR_STARTTLS), me.name, !BadPtr(sptr->name) ? sptr->name : "*", "STARTTLS failed");
 	sptr->ssl = NULL;
 	sptr->flags &= ~FLAGS_SSL;
 	SetUnknown(sptr);
