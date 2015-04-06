3b25ba5aed37bb0cd5a0d600caba4918dac39738
Bram Matthys <syzop@vulnscan.org>
2015-04-06 10:44:56 +0200

    Fix build with LibreSSL / Detect missing RAND_egd. Patch from Bernard Spil.

--- src/random.c
+++ src/random.c
@@ -187,7 +187,7 @@ MEMORYSTATUS mstat;
 
 	/* Grab non-OS specific "random" data */
 #ifdef USE_SSL
- #if OPENSSL_VERSION_NUMBER >= 0x000907000
+ #if OPENSSL_VERSION_NUMBER >= 0x000907000 && defined(HAVE_RAND_EGD)
 	if (EGD_PATH) {
 		n = RAND_query_egd_bytes(EGD_PATH, rdat.egd, sizeof(rdat.egd));
 	}
