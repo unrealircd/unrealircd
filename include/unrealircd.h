8caec780e40b6b6c203199c1d33d23462bdef51c
Bram Matthys <syzop@vulnscan.org>
2015-04-25 13:34:29 +0200

    modules can now simply include "unrealircd.h" rather than include various files in a specific order and risk missing ones..

---
+++ include/unrealircd.h
@@ -0,0 +1,25 @@
+/** Standard include for all UnrealIRCd modules.
+ * This should normally provide all of UnrealIRCd's functionality
+ * (that is publicly exposed anyway).
+ */
+#include "config.h"
+#include "struct.h"
+#include "common.h"
+#include "sys.h"
+#include "numeric.h"
+#include "msg.h"
+#include "proto.h"
+#include "channel.h"
+#include <time.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef _WIN32
+#include <io.h>
+#endif
+#include <fcntl.h>
+#include "h.h"
+#ifdef _WIN32
+#include "version.h"
+#endif
