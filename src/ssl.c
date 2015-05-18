8f7886d9c7ae70c46d85c39d48025ea1f4658fde
Bram Matthys <syzop@vulnscan.org>
2015-05-18 12:12:24 +0200

    init_ctx_client() was accidentally setting options on ctx_server. Reported by Jobe (#4346).

--- src/ssl.c
+++ src/ssl.c
@@ -312,7 +312,7 @@ SSL_CTX *ctx_client;
 		return NULL;
 	}
 	SSL_CTX_set_default_passwd_cb(ctx_client, ssl_pem_passwd_cb);
-	SSL_CTX_set_options(ctx_server, SSL_OP_NO_SSLv2);
+	SSL_CTX_set_options(ctx_client, SSL_OP_NO_SSLv2);
 	SSL_CTX_set_session_cache_mode(ctx_client, SSL_SESS_CACHE_OFF);
 
 	setup_dh_params(ctx_client);
