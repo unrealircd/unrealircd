b0e951e4725433380d4c93f9fb0f925daf0f6b83
Travis McArthur <travis@sectionfive.net>
2015-06-26 13:50:34 -0700

    Add Mac OSX Project

---
+++ src/macosx/UnrealIRCd/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UnrealIRCd
+//
+//  Created by Travis McArthur on 6/26/15.
+//  Copyright (c) 2015 UnrealIRCd Team. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ src/macosx/UnrealIRCd/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UnrealIRCd
+//
+//  Created by Travis McArthur on 6/26/15.
+//  Copyright (c) 2015 UnrealIRCd Team. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
