c461b163d6c021018ac306af582394481e0b07d6
Nathan Phillip Brink <binki@gentoo.org>
2016-08-17 05:43:10 +0000

    Use localstatedir instead of datadir for tunefile, etc.
    datadir is normally /usr/share and specifies the location of files
    that are never modified. localstatedir is normally /var/lib and
    where you are supposed to place files which should be modified.
    This change moves slightly towards doing directories how one
    should when using autoconf and uses the existing --localstatedir
    option, removing the redundant and misused --with-datadir.
    
    The change should be transparent to anyone using ./Config. I have
    also added --with-standard-dirs which would give one directly
    invoking ./configure and passing --localstatedir the results
    they expect (if you passed --localstatedir=/var/lib you’d expect
    unrealircd to store its state in /var/lib/unrealircd).

--- include/config.h
+++ include/config.h
@@ -243,7 +243,7 @@
 #define	LPATH		LOGDIR"/debug.log"	/* Where the debug file lives, if DEBUGMODE */
 #define VPATH		CONFDIR"/ircd.svsmotd"	/* Services MOTD append. */
 #define BPATH		CONFDIR"/bot.motd"	/* Bot MOTD */
-#define IRCDTUNE 	PERMDATADIR"/ircd.tune"	/* tuning .. */
+#define IRCDTUNE 	PKGLOCALSTATEDIR"/ircd.tune"	/* tuning .. */
 
 /* CHROOTDIR
  *
