f473e355e1dc422c4f019dbf86bc50ba1a34a766
Bram Matthys <syzop@vulnscan.org>
2016-09-03 20:08:21 +0200

    Fix AUTHENTICATE bug

--- src/modules/m_sasl.c
+++ src/modules/m_sasl.c
@@ -230,6 +230,12 @@ CMD_FUNC(m_authenticate)
 		return 0;
 	}
 
+	if ((parv[1][0] == ':') || strchr(parv[1], ' '))
+	{
+		sendto_one(sptr, err_str(ERR_CANNOTDOCOMMAND), me.name, "*", "AUTHENTICATE", "Invalid parameter");
+		return 0;
+	}
+
 	if (strlen(parv[1]) > 400)
 	{
 		sendto_one(sptr, err_str(ERR_SASLTOOLONG), me.name, BadPtr(sptr->name) ? "*" : sptr->name);
