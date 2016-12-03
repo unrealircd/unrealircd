41bd7ad4b95469dfa8d1d0b713b07f88604269f6
Bram Matthys <syzop@vulnscan.org>
2016-12-03 10:58:49 +0100

    UnrealIRCd 4.0.9

--- include/win32/setup.h
+++ include/win32/setup.h
@@ -84,10 +84,10 @@
 #define UNREAL_VERSION_MAJOR 0
 
 /* Minor version number (e.g.: 1 for Unreal3.2.1) */
-#define UNREAL_VERSION_MINOR 8
+#define UNREAL_VERSION_MINOR 9
 
 /* Version suffix such as a beta marker or release candidate marker. (e.g.:
    -rcX for unrealircd-3.2.9-rcX) */
-#define UNREAL_VERSION_SUFFIX ".4"
+#define UNREAL_VERSION_SUFFIX ""
 
 #endif
