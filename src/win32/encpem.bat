9dc4e7d31b1c21beb034dfabea58939727e4a5ef
Bram Matthys <syzop@vulnscan.org>
2017-03-08 09:03:03 +0100

    Windows: shut up warning on certificate generation during installation. WARNING: can't open config file: c:/libressl/ssl/openssl.cnf

--- src/win32/encpem.bat
+++ src/win32/encpem.bat
@@ -1,3 +1,4 @@
 @title Encrypting server private key
+SET OPENSSL_CONF=ssl.cnf
 openssl rsa -in conf/ssl/server.key.pem -out conf/ssl/server.key.c.pem -des3
 copy conf/ssl/server.key.c.pem conf/ssl/server.key.pem
--- src/win32/makecert.bat
+++ src/win32/makecert.bat
@@ -1,4 +1,5 @@
 @title Certificate Generation
+SET OPENSSL_CONF=ssl.cnf
 openssl req -new -config ssl.cnf -out conf/ssl/server.req.pem -keyout conf/ssl/server.key.pem -nodes
 openssl req -x509 -config ssl.cnf -days 3650 -sha256 -in conf/ssl/server.req.pem -key conf/ssl/server.key.pem -out conf/ssl/server.cert.pem
 
