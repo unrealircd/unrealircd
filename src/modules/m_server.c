efb344b9b259a113406f588c9d81d2ea46e03ce8
Bram Matthys <syzop@vulnscan.org>
2017-08-19 12:07:54 +0200

    duh.

--- src/modules/m_server.c
+++ src/modules/m_server.c
@@ -358,7 +358,7 @@ skip_host_check:
 				get_client_name(cptr, TRUE));
 		return exit_client(cptr, cptr, &me, "Full class");
 	}
-	if (!IsLocal(cptr) && (iConf.plaintext_policy_server == PLAINTEXT_POLICY_DENY))
+	if (!IsLocal(cptr) && (iConf.plaintext_policy_server == PLAINTEXT_POLICY_DENY) && !IsSecure(cptr))
 	{
 		sendto_one(cptr, "ERROR :Servers need to use SSL/TLS (set::plaintext-policy::server is 'deny')");
 		sendto_realops("Rejected insecure server. See https://www.unrealircd.org/docs/FAQ#ERROR:_Servers_need_to_use_SSL.2FTLS");
