296decf648851e56d8eb81565af705d56b76447b
Bram Matthys <syzop@vulnscan.org>
2017-09-06 16:49:25 +0200

    This code can be removed now that we have a working verify_certificate(). Also broke LibreSSL (SSL_CTX_get0_param undefined).

--- src/crashreport.c
+++ src/crashreport.c
@@ -464,16 +464,6 @@ SSL_CTX *crashreport_init_ssl(void)
 	SSL_CTX_load_verify_locations(ctx_client, buf, NULL);
 	SSL_CTX_set_verify(ctx_client, SSL_VERIFY_PEER, NULL);
 
-#if OPENSSL_VERSION_NUMBER > 0x1000200f
-	/* Enable hostname checks. This is only in OpenSSL 1.0.2+ */
-	param = SSL_CTX_get0_param(ctx_client);
-	X509_VERIFY_PARAM_set1_host(param, CRASH_REPORT_HOST, 0);
-#ifdef X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS
-	/* This one requires even a newer version */
-	X509_VERIFY_PARAM_set_hostflags(param, X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS);
-#endif
-#endif
-
 	/* Limit ciphers as well */
 	SSL_CTX_set_cipher_list(ctx_client, UNREALIRCD_DEFAULT_CIPHERS);
 
