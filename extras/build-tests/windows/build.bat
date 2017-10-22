99f9d1aa7c1fca48a00df8b013e826edd9e1f6ee
Bram Matthys <syzop@vulnscan.org>
2017-10-22 16:02:48 +0200

    AppVeyor: restructuring, add vs2017 target, ..

---
+++ extras/build-tests/windows/build.bat
@@ -0,0 +1,50 @@
+rem Build script for appveyor
+
+rem Initialize Visual Studio variables
+if "%TARGET%" == "Visual Studio 2012" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+if "%TARGET%" == "Visual Studio 2017" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+
+rem Installing tools
+cinst unrar -y
+cinst wget -y
+cinst innosetup -y
+wget https://www.unrealircd.org/files/dev/win/dlltool.exe
+
+rem Installing UnrealIRCd dependencies
+cd \projects
+mkdir unrealircd-deps
+cd unrealircd-deps
+wget https://www.unrealircd.org/files/dev/win/SetACL.exe
+wget https://www.unrealircd.org/files/dev/win/libs/unrealircd-libraries-4.0.8.rar
+unrar x unrealircd-libraries-4.0.8.rar
+
+cd \projects\unrealircd
+
+rem Now the actual build
+call extras\build-tests\windows\makecmd\%SHORTNAME%.bat
+
+rem The above command will fail, due to missing symbol file
+rem However the symbol file can only be generated after the above command
+rem So... we create the symbolfile...
+nmake -f makefile.win32 SYMBOLFILE
+
+rem And we re-run the exact same command:
+call extras\build-tests\windows\makecmd\%SHORTNAME%.bat
+if %ERRORLEVEL% NEQ 0 EXIT /B 1
+
+rem Convert c:\dev to c:\projects\unrealircd-deps
+rem TODO: should use environment variable in innosetup script?
+sed -i "s/c:\\\\dev/c:\\\\projects\\\\unrealircd-deps/gi" src\win32\unrealinst.iss
+
+rem Build installer file
+"c:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc src\win32\unrealinst.iss
+if %ERRORLEVEL% NEQ 0 EXIT /B 1
+
+rem Show some proof
+ren mysetup.exe unrealircd-dev-build.exe
+dir unrealircd-dev-build.exe
+sha256sum unrealircd-dev-build.exe
+
+rem Upload artifact
+appveyor PushArtifact unrealircd-dev-build.exe
+if %ERRORLEVEL% NEQ 0 EXIT /B 1
---
+++ extras/build-tests/windows/compilecmd/vs2017.bat
@@ -0,0 +1,18 @@
+rem Build command for Visual Studio 2017
+
+nmake -f makefile.win32 ^
+LIBRESSL_INC_DIR="c:\projects\unrealircd-deps\libressl\include" ^
+LIBRESSL_LIB_DIR="c:\projects\unrealircd-deps\libressl\x86" ^
+SSLLIB="libcrypto-38.lib libssl-39.lib libtls-11.lib" ^
+USE_REMOTEINC=1 ^
+LIBCURL_INC_DIR="c:\projects\unrealircd-deps\curl-ssl\include" ^
+LIBCURL_LIB_DIR="c:\projects\unrealircd-deps\curl-ssl\builds\libcurl-vc-x86-release-dll-ssl-dll-ipv6-sspi-obj-lib" ^
+CARES_LIB_DIR="c:\projects\unrealircd-deps\c-ares\msvc110\cares\dll-release" ^
+CARES_INC_DIR="c:\projects\unrealircd-deps\c-ares" ^
+CARESLIB="cares.lib" ^
+TRE_LIB_DIR="c:\projects\unrealircd-deps\tre\win32\release" ^
+TRE_INC_DIR="c:\projects\unrealircd-deps\tre" ^
+TRELIB="tre.lib" ^
+PCRE2_INC_DIR="c:\projects\unrealircd-deps\pcre2\build" ^
+PCRE2_LIB_DIR="c:\projects\unrealircd-deps\pcre2\build\release" ^
+PCRE2LIB="pcre2-8.lib"
