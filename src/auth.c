6ef9a83c977e220dab7c389d78e943851a4cfb23
Bram Matthys <syzop@vulnscan.org>
2018-04-22 09:48:12 +0200

    Surely this can use the generic #include "unrealircd.h" instead? Avoids sendto_realops() warning.

--- src/auth.c
+++ src/auth.c
@@ -17,25 +17,7 @@
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-
-#include "struct.h"
-#include "common.h"
-#include "sys.h"
-#include "numeric.h"
-#include "msg.h"
-#include "channel.h"
-#include "version.h"
-#include <time.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#ifdef _WIN32
-#include <io.h>
-#endif
-#include <fcntl.h>
-#include "h.h"
-
+#include "unrealircd.h"
 #include "crypt_blowfish.h"
 
 anAuthStruct MODVAR AuthTypes[] = {
