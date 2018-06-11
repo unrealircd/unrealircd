unrealircd/unrealircd

commit dd6f67a26658409294a75e010ef65700588baacf

Send errors regarding invalid bans (if available). Fix case where conv_param() returns NULL (ban rejected) causing is_ok() function not to be called so the user never sees the error. We now try to call the is_ok after conv_param returns NULL. So not really an API change, more like a fix.

src/modules/m_mode.c
@@ -1069,7 +1069,17 @@ int  do_mode_char(aChannel *chptr, long modetype, char modechar, char *param,
 		  retval = 1;
 		  tmpstr = clean_ban_mask(param, what, cptr);
 		  if (BadPtr(tmpstr))
+		  {
+		  	  /* Invalid ban. See if we can send an error about that */
+		  	  if ((param[0] == '~') && MyClient(cptr) && !bounce)
+		  	  {
+		  	  	Extban *p = findmod_by_bantype(param[1]);
+		  	  	if (p && p->is_ok)
+			  	  	p->is_ok(cptr, chptr, param, EXBCHK_PARAM, what, EXBTYPE_BAN);
+			  }
+		  	  
 		      break; /* ignore ban, but eat param */
+		  }
 		  if ((tmpstr[0] == '~') && MyClient(cptr) && !bounce)
 		  {
 		      /* extban: check access if needed */

commit e456f621efcc8a9f11efd5ba76f653c4682d8cd0

Fix OOB read in extended bans handling. (Bug caused by commit dd6f67a26658409294a75e010ef65700588baacf)

src/modules/m_mode.c
@@ -1065,7 +1065,7 @@ int  do_mode_char(aChannel *chptr, long modetype, char modechar, char *param,
 			if (BadPtr(tmpstr))
 			{
 				/* Invalid ban. See if we can send an error about that */
-				if ((param[0] == '~') && MyClient(cptr) && !bounce)
+				if ((param[0] == '~') && MyClient(cptr) && !bounce && (strlen(param) > 2))
 				{
 					Extban *p = findmod_by_bantype(param[1]);
 					if (p && p->is_ok)
@@ -1113,7 +1113,7 @@ int  do_mode_char(aChannel *chptr, long modetype, char modechar, char *param,
 			tmpstr = clean_ban_mask(param, what, cptr);
 			if (BadPtr(tmpstr))
 				break; /* ignore except, but eat param */
-			if ((tmpstr[0] == '~') && MyClient(cptr) && !bounce)
+			if ((tmpstr[0] == '~') && MyClient(cptr) && !bounce && (strlen(param) > 2))
 			{
 				/* extban: check access if needed */
 				Extban *p = findmod_by_bantype(tmpstr[1]);
@@ -1152,7 +1152,7 @@ int  do_mode_char(aChannel *chptr, long modetype, char modechar, char *param,
 			tmpstr = clean_ban_mask(param, what, cptr);
 			if (BadPtr(tmpstr))
 				break; /* ignore except, but eat param */
-			if ((tmpstr[0] == '~') && MyClient(cptr) && !bounce)
+			if ((tmpstr[0] == '~') && MyClient(cptr) && !bounce && (strlen(param) > 2))
 			{
 				/* extban: check access if needed */
 				Extban *p = findmod_by_bantype(tmpstr[1]);