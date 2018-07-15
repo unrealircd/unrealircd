54a6e2f61f8a4c5dce82b7963b0ef1f38bff7785
Bram Matthys <syzop@vulnscan.org>
2018-07-15 18:11:00 +0200

    Update build command for vs2017 with new deps

--- extras/build-tests/windows/compilecmd/vs2017.bat
+++ extras/build-tests/windows/compilecmd/vs2017.bat
@@ -1,18 +1,18 @@
 rem Build command for Visual Studio 2017
 
 nmake -f makefile.win32 ^
-LIBRESSL_INC_DIR="c:\projects\unrealircd-deps\libressl\include" ^
-LIBRESSL_LIB_DIR="c:\projects\unrealircd-deps\libressl\x86" ^
-SSLLIB="libcrypto-41.lib libssl-43.lib libtls-15.lib" ^
+LIBRESSL_INC_DIR="c:\projectsNREALIRCD-DEPS\libressl\include" ^
+LIBRESSL_LIB_DIR="c:\projectsNREALIRCD-DEPS\libressl\lib" ^
+SSLLIB="crypto-43.lib ssl-45.lib" ^
 USE_REMOTEINC=1 ^
-LIBCURL_INC_DIR="c:\projects\unrealircd-deps\curl-ssl\include" ^
-LIBCURL_LIB_DIR="c:\projects\unrealircd-deps\curl-ssl\builds\libcurl-vc-x86-release-dll-ssl-dll-ipv6-sspi-obj-lib" ^
-CARES_LIB_DIR="c:\projects\unrealircd-deps\c-ares\msvc110\cares\dll-release" ^
-CARES_INC_DIR="c:\projects\unrealircd-deps\c-ares" ^
+LIBCURL_INC_DIR="c:\projectsNREALIRCD-DEPS\curl-ssl\include" ^
+LIBCURL_LIB_DIR="c:\projectsNREALIRCD-DEPS\curl-ssl\builds\libcurl-vc-x86-release-dll-ssl-dll-ipv6-sspi-obj-lib" ^
+CARES_LIB_DIR="c:\projectsNREALIRCD-DEPS\c-ares\msvc\cares\dll-release" ^
+CARES_INC_DIR="c:\projectsNREALIRCD-DEPS\c-ares" ^
 CARESLIB="cares.lib" ^
-TRE_LIB_DIR="c:\projects\unrealircd-deps\tre\win32\release" ^
-TRE_INC_DIR="c:\projects\unrealircd-deps\tre" ^
+TRE_LIB_DIR="c:\projectsNREALIRCD-DEPS\tre\win32\release" ^
+TRE_INC_DIR="c:\projectsNREALIRCD-DEPS\tre" ^
 TRELIB="tre.lib" ^
-PCRE2_INC_DIR="c:\projects\unrealircd-deps\pcre2\build" ^
-PCRE2_LIB_DIR="c:\projects\unrealircd-deps\pcre2\build\release" ^
+PCRE2_INC_DIR="c:\projectsNREALIRCD-DEPS\pcre2\include" ^
+PCRE2_LIB_DIR="c:\projectsNREALIRCD-DEPS\pcre2\lib" ^
 PCRE2LIB="pcre2-8.lib" %*
