unrealircd/unrealircd

commit 51ed51dff1177c0dd860145117167353ac1b89fb

Remove non-standard 'nested comments' features, also known as "comments within comments are not ignored". Reported by bekarfel in https://bugs.unrealircd.org/view.php?id=4075 FAQ entry: https://www.unrealircd.org/docs/FAQ#Nesting_comments

src/s_conf.c
@@ -963,27 +963,14 @@ static ConfigFile *config_parse(char *filename, char *confdata)
 				else if (*(ptr+1) == '*')
 				{
 					int commentstart = linenumber;
-					int commentlevel = 1;
 
 					for(ptr+=2;*ptr;ptr++)
 					{
-						if ((*ptr == '/') && (*(ptr+1) == '*'))
+						if ((*ptr == '*') && (*(ptr+1) == '/'))
 						{
-							commentlevel++;
 							ptr++;
-						}
-
-						else if ((*ptr == '*') && (*(ptr+1) == '/'))
-						{
-							commentlevel--;
-							ptr++;
-						}
-
-						else if (*ptr == '\n')
-							linenumber++;
-
-						if (!commentlevel)
 							break;
+						}
 					}
 					if (!*ptr)
 					{
@@ -1074,6 +1061,8 @@ static ConfigFile *config_parse(char *filename, char *confdata)
 				{
 					config_status("%s:%i Ignoring extra end comment\n",
 						filename, linenumber);
+					config_status("WARNING: Starting with UnrealIRCd 4.2.1 a /*-style comment stops as soon as the first */ is encountered. "
+					              "See https://www.unrealircd.org/docs/FAQ#Nesting_comments for more information.");
 					ptr++;
 					break;
 				}

commit 0ac56e44447f402df05941f9eb92e1df370e2d3f

Fix line number in error messages being off, as reported in https://bugs.unrealircd.org/view.php?id=5169 caused by commit 51ed51dff1177c0dd860145117167353ac1b89fb

src/s_conf.c
@@ -975,6 +975,10 @@ static ConfigFile *config_parse(char *filename, char *confdata)
 
 					for(ptr+=2;*ptr;ptr++)
 					{
+						if (*ptr == '\n')
+						{
+							linenumber++;
+						} else
 						if ((*ptr == '*') && (*(ptr+1) == '/'))
 						{
 							ptr++;