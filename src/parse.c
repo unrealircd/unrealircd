8c9e4b866850b630d7941a6db398e31ccd880f9e
Bram Matthys <syzop@vulnscan.org>
2019-01-06 19:21:59 +0100

    Poison unused parv[] elements that code should never access. The last parv[] array element will be NULL. Accessing any elements after that is undefined, similar to reading past the nul byte of a string. This poison will help catch such bugs. Without this poison your code will also crash, now it just crashes more consistently.

--- src/parse.c
+++ src/parse.c
@@ -205,12 +205,17 @@ int  parse(aClient *cptr, char *buffer, char *bufend)
 		return FLUSH_BUFFER;
 	}
 
-	/* this call is a bit obsolete? - takes up CPU */
-	backupbuf[0] = '\0';
+	/* This stores the last executed command in 'backupbuf', useful for debugging crashes */
 	strlcpy(backupbuf, buffer, sizeof(backupbuf));
+
 #if defined(DEBUGMODE) && defined(RAWCMDLOGGING)
 	ircd_log(LOG_ERROR, "<- %s: %s", cptr->name, backupbuf);
 #endif
+
+	/* This poisons unused para elements that code should never access */
+	for (i = 0; i < MAXPARA+2; i++)
+		para[i] = (char *)0xDEADBEEF;
+
 	s = sender;
 	*s = '\0';
 	for (ch = buffer; *ch == ' '; ch++)
