unrealircd/unrealircd

commit dde8f914fbda1c974adb3a18a81a9a1b43c2eea1

Internal: make UID available early (pre-auth).

src/list.c
@@ -144,6 +144,7 @@ aClient *make_client(aClient *from, aClient *servr)
 	if (!from)
 	{
 		/* Local client */
+		const char *id;
 		
 		cptr->local = MyMallocEx(sizeof(aLocalClient));
 		
@@ -161,6 +162,11 @@ aClient *make_client(aClient *from, aClient *servr)
 
 		dbuf_queue_init(&cptr->local->recvQ);
 		dbuf_queue_init(&cptr->local->sendQ);
+
+		while (hash_find_id((id = uid_get()), NULL) != NULL)
+			;
+		strlcpy(cptr->id, id, sizeof cptr->id);
+		add_to_id_hash_table(cptr->id, cptr);
 	} else {
 		cptr->fd = -256;
 	}

src/modules/m_nick.c
@@ -1393,11 +1393,6 @@ int _register_user(aClient *cptr, aClient *sptr, char *nick, char *username, cha
 
 		list_move(&sptr->lclient_node, &lclient_list);
 
-		while (hash_find_id((id = uid_get()), NULL) != NULL)
-			;
-		strlcpy(sptr->id, id, sizeof sptr->id);
-		(void)add_to_id_hash_table(sptr->id, sptr);
-
 		IRCstats.unknown--;
 		IRCstats.me_clients++;
 

src/modules/m_protoctl.c
@@ -285,7 +285,7 @@ CMD_FUNC(m_protoctl)
 			if (!IsServer(cptr) && !IsEAuth(cptr) && !IsHandshake(cptr))
 				return exit_client(cptr, cptr, &me, "Got PROTOCTL SID before EAUTH, that's the wrong order!");
 
-			if (*sptr->id)
+			if (*sptr->id && (strlen(sptr->id)==3))
 				return exit_client(cptr, cptr, &me, "Got PROTOCTL SID twice");
 
 			if ((acptr = hash_find_id(sid, NULL)) != NULL)
@@ -296,8 +296,10 @@ CMD_FUNC(m_protoctl)
 				return exit_client(cptr, cptr, &me, "SID collision");
 			}
 
+			if (*sptr->id)
+				del_from_id_hash_table(sptr->id, sptr); /* delete old UID entry (created on connect) */
 			strlcpy(cptr->id, sid, IDLEN);
-			add_to_id_hash_table(cptr->id, cptr);
+			add_to_id_hash_table(cptr->id, cptr); /* add SID */
 			cptr->local->proto |= PROTO_SID;
 		}
 		else if ((strncmp(s, "EAUTH=", 6) == 0) && NEW_LINKING_PROTOCOL)

commit 3712fad8912b4bacb95b22ca54cad484441cc9c2

When a server does not use SID's, set empty id as before. This bug was post-rc1, caused by dde8f914fbda1c974adb3a18a81a9a1b43c2eea1.

src/list.c
@@ -271,6 +271,14 @@ aServer *make_server(aClient *cptr)
 		serv->up = NULL;
 		cptr->serv = serv;
 	}
+	if (strlen(cptr->id) > 3)
+	{
+		/* Probably the auto-generated UID for a server that
+		 * still uses the old protocol (without SID).
+		 */
+		del_from_id_hash_table(cptr->id, cptr);
+		*cptr->id = '\0';
+	}
 	return cptr->serv;
 }
 