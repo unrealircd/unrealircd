35600fdfa09e29ad9b5c742c06421441747a8c63
GottemHams <dev@gottem.nl>
2019-05-28 21:34:11 +0200

    Updated storetkl: write DB on a timer instead of hooking TKL_ADD/DEL, better DB format for improved compatibility with future Unreal versions, configurable .db path, support for softbans, write DB to a tempfile first to prevent corruption of existing DB, also applied some misc optimisations/portability improvements

--- src/modules/m_storetkl.c
+++ src/modules/m_storetkl.c
@@ -19,7 +19,8 @@
 
 #include "unrealircd.h"
 
-#define TKL_DB_VERSION 1000
+#define TKL_DB_VERSION 1100
+#define TKL_DB_SAVE_EVERY 893
 
 // Some macros
 #ifndef _WIN32
@@ -28,25 +29,36 @@
 	#define OpenFile(fd, file, flags) fd = open(file, flags, S_IREAD | S_IWRITE)
 #endif
 
+#define FreeTKLRead() \
+ 	do { \
+		/* Some of these might be NULL */ \
+		if(tkltype) MyFree(tkltype); \
+		if(usermask) MyFree(usermask); \
+		if(hostmask) MyFree(hostmask); \
+		if(reason) MyFree(reason); \
+		if(setby) MyFree(setby); \
+		if(spamf_check) MyFree(spamf_check); \
+		if(spamf_matchtype) MyFree(spamf_matchtype); \
+	} while(0)
+
 #define R_SAFE(x) \
 	do { \
-		if((x)) \
-		{ \
+		if((x)) { \
 			close(fd); \
-			config_warn("[storetkl] Read error from the persistent storage file '%s' on server %s (possible corruption)", cfg.database, me.name); \
+			config_warn("[storetkl] Read error from the persistent storage file '%s' (possible corruption)", cfg.database); \
+			FreeTKLRead(); \
 			return -1; \
 		} \
-	} while (0)
+	} while(0)
 
 #define W_SAFE(x) \
 	do { \
-		if((x)) \
-		{ \
+		if((x)) { \
 			close(fd); \
-			config_warn("[storetkl] Write error from the persistent storage file '%s' on server %s (possible corruption)", cfg.database, me.name); \
+			config_warn("[storetkl] Write error from the persistent storage file '%s' (DATABASE NOT SAVED)", cfg.database); \
 			return -1; \
 		} \
-	} while (0)
+	} while(0)
 
 #define IsMDErr(x, y, z) \
 	do { \
@@ -58,14 +70,15 @@
 
 // Forward declarations
 void storetkl_moddata_free(ModData *md);
-int storetkl_hook_tkl_add(aClient *cptr, aClient *sptr, aTKline *tkl, int parc, char *parv[]);
-int storetkl_hook_tkl_del(aClient *cptr, aClient *sptr, aTKline *tkl, int parc, char *parv[]);
 void setcfg(void);
 void freecfg(void);
+int storetkl_configtest(ConfigFile *cf, ConfigEntry *ce, int type, int *errs);
+int storetkl_configrun(ConfigFile *cf, ConfigEntry *ce, int type);
+EVENT(writeDB_evt);
+int writeDB(void);
 int readDB(void);
-int writeDB(aTKline *origtkl, char what);
-static inline int read_data(int fd, void *buf, size_t count);
-static inline int write_data(int fd, void *buf, size_t count);
+static inline int read_data(int fd, void *buf, size_t len);
+static inline int write_data(int fd, void *buf, size_t len);
 static int write_str(int fd, char *x);
 static int read_str(int fd, char **x);
 
@@ -76,14 +89,21 @@ struct cfgstruct {
 	char *database;
 };
 static struct cfgstruct cfg;
+unsigned backport_tkl1000; // Stored .db might still work with flags instead of actual values (will be corrected on next write)
 
 ModuleHeader MOD_HEADER(m_storetkl) = {
 	"m_storetkl",
-	"v1.02",
+	"v1.10",
 	"Store TKL entries persistently across IRCd restarts",
 	"3.2-b8-1",
 	NULL
 };
+
+MOD_TEST(m_storetkl) {
+	memset(&cfg, 0, sizeof(cfg));
+	HookAdd(modinfo->handle, HOOKTYPE_CONFIGTEST, 0, storetkl_configtest);
+	return MOD_SUCCESS;
+}
 
 MOD_INIT(m_storetkl) {
 	MARK_AS_OFFICIAL_MODULE(modinfo);
@@ -105,12 +125,12 @@ MOD_INIT(m_storetkl) {
 		readDB();
 		moddata_client((&me), storetkl_md).i = 1;
 	}
-	HookAdd(modinfo->handle, HOOKTYPE_TKL_ADD, 999, storetkl_hook_tkl_add);
-	HookAdd(modinfo->handle, HOOKTYPE_TKL_DEL, 999, storetkl_hook_tkl_del);
+	HookAdd(modinfo->handle, HOOKTYPE_CONFIGRUN, 0, storetkl_configrun);
 	return MOD_SUCCESS;
 }
 
 MOD_LOAD(m_storetkl) {
+	EventAddEx(modinfo->handle, "storetkl_writedb", TKL_DB_SAVE_EVERY, 0, writeDB_evt, NULL);
 	if(ModuleGetError(modinfo->handle) != MODERR_NOERROR) {
 		config_error("A critical error occurred when loading module %s: %s", MOD_HEADER(m_storetkl).name, ModuleGetErrorStr(modinfo->handle));
 		return MOD_FAILED;
@@ -119,6 +139,7 @@ MOD_LOAD(m_storetkl) {
 }
 
 MOD_UNLOAD(m_storetkl) {
+	writeDB();
 	freecfg();
 	return MOD_SUCCESS;
 }
@@ -128,50 +149,116 @@ void storetkl_moddata_free(ModData *md) {
 		md->i = 0;
 }
 
-int storetkl_hook_tkl_add(aClient *cptr, aClient *sptr, aTKline *tkl, int parc, char *parv[]) {
-	writeDB(tkl, '+');
-	return HOOK_CONTINUE;
-}
-int storetkl_hook_tkl_del(aClient *cptr, aClient *sptr, aTKline *tkl, int parc, char *parv[]) {
-	writeDB(tkl, '-');
-	return HOOK_CONTINUE;
+void setcfg(void) {
+	// Default: data/tkl.db
+	cfg.database = strdup("tkl.db");
+	convert_to_absolute_path(&cfg.database, PERMDATADIR);
 }
 
-int writeDB(aTKline *origtkl, char what) {
+void freecfg(void) {
+	MyFree(cfg.database);
+}
+
+int storetkl_configtest(ConfigFile *cf, ConfigEntry *ce, int type, int *errs) {
+	int errors = 0;
+	ConfigEntry *cep;
+
+	// We are only interested in set::storetkl::database
+	if(type != CONFIG_SET)
+		return 0;
+
+	if(!ce || strcmp(ce->ce_varname, "storetkl"))
+		return 0;
+
+	for(cep = ce->ce_entries; cep; cep = cep->ce_next) {
+		if(!cep->ce_vardata) {
+			config_error("%s:%i: blank set::storetkl::%s without value", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, cep->ce_varname);
+			errors++;
+			continue;
+		}
+		if(!strcmp(cep->ce_varname, "database")) {
+			convert_to_absolute_path(&cep->ce_vardata, PERMDATADIR);
+			continue;
+		}
+		config_error("%s:%i: unknown directive set::storetkl::%s", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, cep->ce_varname);
+		errors++;
+	}
+
+	*errs = errors;
+	return errors ? -1 : 1;
+}
+
+int storetkl_configrun(ConfigFile *cf, ConfigEntry *ce, int type) {
+	ConfigEntry *cep;
+
+	// We are only interested in set::storetkl::database
+	if(type != CONFIG_SET)
+		return 0;
+
+	if(!ce || strcmp(ce->ce_varname, "storetkl"))
+		return 0;
+
+	for(cep = ce->ce_entries; cep; cep = cep->ce_next) {
+		if(!strcmp(cep->ce_varname, "database"))
+			safestrdup(cfg.database, cep->ce_vardata);
+	}
+	return 1;
+}
+
+EVENT(writeDB_evt) {
+	writeDB();
+}
+
+int writeDB(void) {
+	char tmpfname[512];
 	int fd;
-	size_t count;
+	uint64_t tklcount;
 	int index;
 	aTKline *tkl;
+	char *tkltype = NULL;
+	char usermask_subtype[256]; // Might need to prefix something to usermask
 
-	OpenFile(fd, cfg.database, O_CREAT | O_WRONLY | O_TRUNC);
+	// Write to a tempfile first, then rename it if everything succeeded
+	snprintf(tmpfname, sizeof(tmpfname), "%s.tmp", cfg.database);
+	OpenFile(fd, tmpfname, O_CREAT | O_WRONLY | O_TRUNC);
 	if(fd == -1) {
-		config_warn("[storetkl] Unable to open the persistent storage file '%s' for writing on server %s: %s", cfg.database, me.name, strerror(errno));
+		config_warn("[storetkl] Unable to open the persistent storage file '%s' for writing: %s", cfg.database, strerror(errno));
 		return -1;
 	}
 
 	W_SAFE(write_data(fd, &tkl_db_version, sizeof(tkl_db_version)));
-
-	count = 0;
+	tklcount = 0;
 	for(index = 0; index < TKLISTLEN; index++) {
 		for(tkl = tklines[index]; tkl; tkl = tkl->next) {
-			if(origtkl && what == '-' && origtkl == tkl)
+			// Local spamfilter means it was added through the conf or is built-in, so let's not even write those
+			if((tkl->type & TKL_SPAMF) && !(tkl->type & TKL_GLOBAL))
 				continue;
-			count++;
+			tklcount++;
 		}
 	}
 
-	W_SAFE(write_data(fd, &count, sizeof(count)));
-
+	W_SAFE(write_data(fd, &tklcount, sizeof(tklcount)));
+	tkltype = malloc(sizeof(char) * 2); // No need to keep reallocating it in a loop
+	tkltype[1] = '\0';
 	for(index = 0; index < TKLISTLEN; index++) {
 		for(tkl = tklines[index]; tkl; tkl = tkl->next) {
-			if(origtkl && what == '-' && origtkl == tkl)
-				continue;
-
 			// Since we can't just write 'tkl' in its entirety, we have to get the relevant variables instead
 			// These will be used to reconstruct the proper internal m_tkl() call ;]
-			W_SAFE(write_data(fd, &tkl->type, sizeof(tkl->type))); // Integer (G:Line, Q:Line, etc; also refer to TKL_*)
-			W_SAFE(write_data(fd, &tkl->subtype, sizeof(tkl->subtype))); // Unsigned short (only used for spamfilters but set to 0 for everything else regardless)
-			W_SAFE(write_str(fd, tkl->usermask)); // User mask (targets for spamfilter, like 'cp')
+			if((tkl->type & TKL_SPAMF) && !(tkl->type & TKL_GLOBAL)) // Also need to skip this here
+				continue;
+			tkltype[0] = tkl_typetochar(tkl->type);
+
+			W_SAFE(write_str(fd, tkltype)); // TKL char
+			W_SAFE(write_data(fd, &tkl->subtype, sizeof(tkl->subtype))); // Unsigned short (only used for spamfilters/softbans but set to 0 for everything else regardless)
+
+			// Might be a softban
+			if(!tkl->ptr.spamf && (tkl->subtype & TKL_SUBTYPE_SOFT)) {
+				snprintf(usermask_subtype, sizeof(usermask_subtype), "%%%s", tkl->usermask);
+				W_SAFE(write_str(fd, usermask_subtype)); // User mask (targets for spamfilter, like 'cp'), includes % for softbans
+			}
+			else
+				W_SAFE(write_str(fd, tkl->usermask));
+
 			W_SAFE(write_str(fd, tkl->hostmask)); // Host mask (action for spamfilter, like 'block')
 			W_SAFE(write_str(fd, tkl->reason)); // Ban reason (TKL time for spamfilters, in case of a gline action)
 			W_SAFE(write_str(fd, tkl->setby));
@@ -184,79 +271,93 @@ int writeDB(aTKline *origtkl, char what) {
 				W_SAFE(write_str(fd, tkl->ptr.spamf->tkl_reason));
 				W_SAFE(write_data(fd, &tkl->ptr.spamf->tkl_duration, sizeof(tkl->ptr.spamf->tkl_duration)));
 				W_SAFE(write_str(fd, tkl->ptr.spamf->expr->str)); // Actual expression/regex/etc
-				W_SAFE(write_data(fd, &tkl->ptr.spamf->expr->type, sizeof(tkl->ptr.spamf->expr->type))); // Integer (expression type [simple/PCRE]; see also enum MatchType)
+
+				// Expression type (simple/PCRE), see also enum MatchType
+				if(tkl->ptr.spamf->expr->type == MATCH_PCRE_REGEX)
+					W_SAFE(write_str(fd, "regex"));
+				else
+					W_SAFE(write_str(fd, "simple"));
 			}
 			else
 				W_SAFE(write_str(fd, "NOSPAMF"));
 		}
 	}
+	MyFree(tkltype);
 
+	// Everything seems to have gone well, attempt to rename the tempfile
 	close(fd);
+	if(rename(tmpfname, cfg.database) < 0) {
+		config_warn("[storetkl] Error renaming '%s' to '%s': %s (DATABASE NOT SAVED)", tmpfname, cfg.database, strerror(errno));
+		return -1;
+	}
 	return 0;
-}
-
-void setcfg(void) {
-	// data/tkl.db
-	cfg.database = strdup("tkl.db");
-	convert_to_absolute_path(&cfg.database, PERMDATADIR);
-}
-
-void freecfg(void) {
-	MyFree(cfg.database);
 }
 
 int readDB(void) {
 	int fd;
-	size_t count;
-	int i;
+	uint64_t i;
+	uint64_t tklcount = 0;
+	size_t tklcount_tkl1000 = 0;
 	unsigned num = 0;
 	unsigned rewrite = 0;
 	unsigned version;
 
+	char *tkltype = NULL;
+	char *usermask = NULL;
+	char *hostmask = NULL;
+	char *reason = NULL;
+	char *setby = NULL;
+	char *spamf_check = NULL;
+	char *spamf_matchtype = NULL;
+
 	ircd_log(LOG_ERROR, "[storetkl] Reading stored X:Lines from '%s'", cfg.database);
 	sendto_realops("[storetkl] Reading stored X:Lines from '%s'", cfg.database); // Probably won't be seen ever, but just in case ;]
 	OpenFile(fd, cfg.database, O_RDONLY);
-
 	if(fd == -1) {
 		if(errno != ENOENT)
-			config_warn("[storetkl] Unable to open the persistent storage file '%s' for reading on server %s: %s", cfg.database, me.name, strerror(errno));
+			config_warn("[storetkl] Unable to open the persistent storage file '%s' for reading: %s", cfg.database, strerror(errno));
 		return -1;
 	}
 
 	R_SAFE(read_data(fd, &version, sizeof(version)));
-
 	if(version > tkl_db_version) { // Older DBs should still work with newer versions of this module
-		config_warn("File '%s' has a wrong database version on server %s: expected it to be <= %u but got %u instead", cfg.database, me.name, tkl_db_version, version);
+		config_warn("[storetkl] File '%s' has a wrong database version: expected it to be <= %u but got %u instead", cfg.database, tkl_db_version, version);
 		close(fd);
 		return -1;
 	}
 
-	R_SAFE(read_data(fd, &count, sizeof(count)));
+	backport_tkl1000 = (version <= 1000 ? 1 : 0);
+	if(backport_tkl1000) {
+		R_SAFE(read_data(fd, &tklcount_tkl1000, sizeof(tklcount_tkl1000)));
+		tklcount = tklcount_tkl1000;
+	}
+	else
+		R_SAFE(read_data(fd, &tklcount, sizeof(tklcount)));
 
-	for(i = 1; i <= count; i++) {
+	for(i = 1; i <= tklcount; i++) {
 		int type;
 		unsigned short subtype;
 		int parc = 0;
 
 		// Variables for all TKL types
-		char *usermask = NULL;
-		char *hostmask = NULL;
-		char *reason = NULL;
-		char *setby = NULL;
+		usermask = NULL;
+		hostmask = NULL;
+		reason = NULL;
+		setby = NULL;
 		char tklflag;
-		char *tkltype = NULL;
+		tkltype = NULL;
 		TS expire_at, set_at;
 		char setTime[100], expTime[100], spamfTime[100];
 
 		// Some stuff related to spamfilters
-		char *spamf_check = NULL;
+		spamf_check = NULL;
 		int spamf = 0;
 		unsigned short spamf_action;
 		char *spamf_tkl_reason = NULL;
 		TS spamf_tkl_duration;
 		char *spamf_expr = NULL;
 		MatchType matchtype;
-		char *spamf_matchtype;
+		spamf_matchtype = NULL;
 
 		int doadd = 1;
 		aTKline *tkl;
@@ -277,7 +378,18 @@ int readDB(void) {
 			NULL, // 12: Some functions rely on the post-last entry being NULL =]
 		};
 
-		R_SAFE(read_data(fd, &type, sizeof(type)));
+		if(backport_tkl1000) {
+			R_SAFE(read_data(fd, &type, sizeof(type)));
+			tkltype = malloc(sizeof(char) * 2);
+			tklflag = tkl_typetochar(type);
+			tkltype[0] = tklflag;
+			tkltype[1] = '\0';
+		}
+		else {
+			R_SAFE(read_str(fd, &tkltype)); // No need for tkl_typetochar() on read anymore
+			tklflag = tkltype[0];
+		}
+
 		R_SAFE(read_data(fd, &subtype, sizeof(subtype)));
 		R_SAFE(read_str(fd, &usermask));
 		R_SAFE(read_str(fd, &hostmask));
@@ -286,39 +398,42 @@ int readDB(void) {
 		R_SAFE(read_data(fd, &expire_at, sizeof(expire_at)));
 		R_SAFE(read_data(fd, &set_at, sizeof(set_at)));
 		R_SAFE(read_str(fd, &spamf_check));
-
 		if(!strcmp(spamf_check, "SPAMF")) {
 			spamf = 1;
 			R_SAFE(read_data(fd, &spamf_action, sizeof(spamf_action)));
 			R_SAFE(read_str(fd, &spamf_tkl_reason));
 			R_SAFE(read_data(fd, &spamf_tkl_duration, sizeof(spamf_tkl_duration)));
 			R_SAFE(read_str(fd, &spamf_expr));
-			R_SAFE(read_data(fd, &matchtype, sizeof(matchtype)));
+			if(backport_tkl1000) {
+				R_SAFE(read_data(fd, &matchtype, sizeof(matchtype)));
+				if(matchtype == MATCH_PCRE_REGEX)
+					spamf_matchtype = strdup("regex");
+				else
+					spamf_matchtype = strdup("simple");
+			}
+			else {
+				// We have better compatibility by just using the strings, since its MATCH_* counterpart might just change in value someday
+				R_SAFE(read_str(fd, &spamf_matchtype));
+				if(!strcmp(spamf_matchtype, "regex"))
+					matchtype = MATCH_PCRE_REGEX;
+				else
+					matchtype = MATCH_SIMPLE;
+			}
 		}
-
-		tkltype = malloc(sizeof(char) * 2);
-		tklflag = tkl_typetochar(type);
-		tkltype[0] = tklflag;
-		tkltype[1] = '\0';
 
 		// Should probably not re-add if it should be expired to begin with
 		if(expire_at != 0 && expire_at <= TStime()) {
 			ircd_log(LOG_ERROR, "[storetkl] Not re-adding %c:Line '%s@%s' [%s] because it should be expired", tklflag, usermask, hostmask, reason);
 			sendto_realops("[storetkl] Not re-adding %c:Line '%s@%s' [%s] because it should be expired", tklflag, usermask, hostmask, reason); // Probably won't be seen ever, but just in case ;]
 			rewrite++;
-			free(tkltype);
-			if(spamf_check) free(spamf_check);
-			if(usermask) free(usermask);
-			if(hostmask) free(hostmask);
-			if(reason) free(reason);
-			if(setby) free(setby);
+			FreeTKLRead();
 			continue;
 		}
 
 		ircsnprintf(setTime, sizeof(setTime), "%li", set_at);
 		ircsnprintf(expTime, sizeof(expTime), "%li", expire_at);
 
-		if(spamf && tklflag == 'f') // Lowercase 'f' means it was added through the conf or is built-in, so let's not touch those at all
+		if(spamf && tklflag == 'f') // Just in case someone modifies the DB to contain local spamfilters, as well as for v1000
 			doadd = 0;
 
 		// Build TKL args
@@ -348,10 +463,6 @@ int readDB(void) {
 				ircsnprintf(spamfTime, sizeof(spamfTime), "%li", spamf_tkl_duration);
 				tkllayer[8] = spamfTime;
 				tkllayer[9] = spamf_tkl_reason;
-				if(matchtype == MATCH_PCRE_REGEX)
-					spamf_matchtype = "regex";
-				else
-					spamf_matchtype = "simple";
 				tkllayer[10] = spamf_matchtype;
 				tkllayer[11] = spamf_expr;
 			}
@@ -369,15 +480,8 @@ int readDB(void) {
 			m_tkl(&me, &me, NULL, parc, tkllayer);
 			num++;
 		}
-
-		free(tkltype);
-		if(spamf_check) free(spamf_check);
-		if(usermask) free(usermask);
-		if(hostmask) free(hostmask);
-		if(reason) free(reason);
-		if(setby) free(setby);
+		FreeTKLRead();
 	}
-
 	close(fd);
 
 	if(num) {
@@ -388,51 +492,60 @@ int readDB(void) {
 	if(rewrite) {
 		ircd_log(LOG_ERROR, "[storetkl] Rewriting DB file due to %d skipped/expired X:Line%s", rewrite, (rewrite > 1 ? "s" : ""));
 		sendto_realops("[storetkl] Rewriting DB file due to %d skipped/expired X:Line%s", rewrite, (rewrite > 1 ? "s" : "")); // Probably won't be seen ever, but just in case ;]
-		return writeDB(NULL, 0);
+		return writeDB();
 	}
 
 	return 0;
 }
 
-static inline int read_data(int fd, void *buf, size_t count) {
-	if((size_t)read(fd, buf, count) < count)
+static inline int read_data(int fd, void *buf, size_t len) {
+	if((size_t)read(fd, buf, len) < len)
 		return -1;
 	return 0;
 }
 
-static inline int write_data(int fd, void *buf, size_t count) {
-	if((size_t)write(fd, buf, count) < count)
+static inline int write_data(int fd, void *buf, size_t len) {
+	if((size_t)write(fd, buf, len) < len)
 		return -1;
 	return 0;
 }
 
 static int write_str(int fd, char *x) {
-	size_t count = x ? strlen(x) : 0;
-	if(write_data(fd, &count, sizeof count))
+	uint64_t len = (x ? strlen(x) : 0);
+	if(write_data(fd, &len, sizeof(len)))
 		return -1;
-	if(count) {
-		if(write_data(fd, x, sizeof(char) * count))
+	if(len) {
+		if(write_data(fd, x, sizeof(char) * len))
 			return -1;
 	}
 	return 0;
 }
 
 static int read_str(int fd, char **x) {
-	size_t count;
-	if(read_data(fd, &count, sizeof count))
-		return -1;
-	if(!count) {
+	uint64_t len;
+	size_t len_tkl1000; // len used to be of type size_t, but this has portability problems
+	size_t size;
+	if(backport_tkl1000) {
+		if(read_data(fd, &len_tkl1000, sizeof(len_tkl1000)))
+			return -1;
+		len = len_tkl1000;
+	}
+	else {
+		if(read_data(fd, &len, sizeof(len)))
+			return -1;
+	}
+	if(!len) {
 		*x = NULL;
 		return 0;
 	}
 
-	*x = (char *)MyMalloc(sizeof(char) * count + 1);
-	if(read_data(fd, *x, sizeof(char) * count)) {
+	size = sizeof(char) * len;
+	*x = (char *)MyMalloc(size + sizeof(char));
+	if(read_data(fd, *x, size)) {
 		MyFree(*x);
 		*x = NULL;
 		return -1;
 	}
-	(*x)[count] = 0;
-
+	(*x)[len] = 0;
 	return 0;
 }
