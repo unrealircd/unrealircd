fea09b6659a0a19e5f5b51996f4c83fe2a253c24
Bram Matthys <syzop@vulnscan.org>
2019-06-30 10:53:58 +0200

    Switch from RSA 4096 to ECC secp384r1

--- src/win32/makecert.bat
+++ src/win32/makecert.bat
@@ -1,5 +1,6 @@
 @title Certificate Generation
 SET OPENSSL_CONF=ssl.cnf
-openssl req -new -config ssl.cnf -out conf/ssl/server.req.pem -keyout conf/ssl/server.key.pem -nodes
+openssl ecparam -out server.key.pem -name secp384r1 -genkey
+openssl req -new -config ssl.cnf -out conf/ssl/server.req.pem -key conf/ssl/server.key.pem -nodes
 openssl req -x509 -config ssl.cnf -days 3650 -sha256 -in conf/ssl/server.req.pem -key conf/ssl/server.key.pem -out conf/ssl/server.cert.pem
 
