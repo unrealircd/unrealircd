3d7117ea77b25c7da6e36636018a02291e1e7019
Bram Matthys <syzop@vulnscan.org>
2019-08-11 17:56:25 +0200

    Whatever this is, I'm not leaving it alone :D @skipci

--- src/windows/leavealone.h
+++
@@ -1,9 +0,0 @@
-#define pb 5
-#define rb 4
-#define mi 1
-#define ma 3
-#define vFILEVERSION ma,mi,rb,pb
-#define vPRODUCTVERSION ma,mi,0,0
-#define vDISPFILEVERSION "3,1,4,5\0"
-#define vSUBBUILD "5\0"
-\0"
