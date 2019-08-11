a53316d256386007140429dd77e1280999a646b7
Bram Matthys <syzop@vulnscan.org>
2019-08-11 17:19:42 +0200

    Rename src/s_svs.c to src/aliases.c, since that is what it contains.

--- src/aliases.c
+++ src/aliases.c
@@ -1,5 +1,5 @@
 /*
- *   Unreal Internet Relay Chat Daemon, src/svs.c
+ *   Unreal Internet Relay Chat Daemon, src/aliases.c
  *   (C) 2000-2001 Carsten V. Munk and the UnrealIRCd Team
  *
  *   This program is free software; you can redistribute it and/or modify
