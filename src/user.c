1a305b0b45715a85a226b327187f2a05ab89c5dd
Bram Matthys <syzop@vulnscan.org>
2019-08-18 08:06:08 +0200

    Remove this old code in hunt_server() or we end up sending message tags twice. (This code was there before sendto_one() got it's extra MessageTag * parameter)

--- src/user.c
+++ src/user.c
@@ -217,21 +217,9 @@ int hunt_server(aClient *cptr, aClient *sptr, MessageTag *mtags, char *command, 
 	saved = parv[server];
 	parv[server] = acptr->name;
 
-	if (mtags)
-	{
-		char *str = mtags_to_string(mtags, acptr);
-		char newcommand[512];
-		/* Update the format string */
-		snprintf(newcommand, sizeof(newcommand), "@%%s %s", command);
-		/* And do the sendto_one() */
-		sendto_one(acptr, mtags, newcommand, str, sptr->name,
-		    parv[1], parv[2], parv[3], parv[4],
-		    parv[5], parv[6], parv[7], parv[8]);
-	} else {
-		sendto_one(acptr, mtags, command, sptr->name,
-		    parv[1], parv[2], parv[3], parv[4],
-		    parv[5], parv[6], parv[7], parv[8]);
-	}
+	sendto_one(acptr, mtags, command, sptr->name,
+	    parv[1], parv[2], parv[3], parv[4],
+	    parv[5], parv[6], parv[7], parv[8]);
 
 	parv[server] = saved;
 
