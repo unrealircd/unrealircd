84918ac68f920b057491880956138a49ef5d6bf7
Bram Matthys <syzop@vulnscan.org>
2019-10-25 11:19:18 +0200

    Remove src/md5.c, integrate those two functions in src/misc.c

--- src/misc.c
+++ src/misc.c
@@ -1568,3 +1568,36 @@ int read_str(FILE *fd, char **x)
 	(*x)[len] = 0;
 	return 1;
 }
+
+/** Generates an MD5 checksum.
+ * @param mdout[out] Buffer to store result in, the result will be 16 bytes in binary
+ *                   (not ascii printable!).
+ * @param src[in]    The input data used to generate the checksum.
+ * @param n[in]      Length of data.
+ */
+void DoMD5(char *mdout, const char *src, unsigned long n)
+{
+	MD5_CTX hash;
+
+	MD5_Init(&hash);
+	MD5_Update(&hash, src, n);
+	MD5_Final(mdout, &hash);
+}
+
+/** Generates an MD5 checksum - ASCII printable string (0011223344..etc..).
+ * @param dst[out]  Buffer to store result in, this will be the result will be
+ *                  32 characters + nul terminator, so needs to be at least 33 characters.
+ * @param src[in]   The input data used to generate the checksum.
+ * @param n[in]     Length of data.
+ */
+char *md5hash(char *dst, const char *src, unsigned long n)
+{
+	char tmp[16];
+
+	DoMD5(tmp, src, n);
+	sprintf(dst, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+		tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6], tmp[7], tmp[8],
+		tmp[9], tmp[10], tmp[11], tmp[12], tmp[13], tmp[14], tmp[15]);
+
+	return dst;
+}
