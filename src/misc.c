28bf68f39b077d30551a7133839d434a9ed716a0
Bram Matthys <syzop@vulnscan.org>
2021-03-08 09:53:28 +0100

    More than 100 lines within an if when you can use if ! w/continue.. pff.

--- src/misc.c
+++ src/misc.c
@@ -152,75 +152,50 @@ void ircd_log(int flags, FORMAT_STRING(const char *format), ...)
 		return;
 	}
 
-	for (logs = conf_log; logs; logs = logs->next) {
+	for (logs = conf_log; logs; logs = logs->next)
+	{
+		if (!(logs->flags & flags))
+			continue;
+
 #ifdef HAVE_SYSLOG
-		if (!strcasecmp(logs->file, "syslog") && logs->flags & flags) {
+		if (!strcasecmp(logs->file, "syslog"))
+		{
 			syslog(LOG_INFO, "%s", buf);
 			written++;
 			continue;
 		}
 #endif
-		if (logs->flags & flags)
+		if (logs->maxsize && (stat(logs->file, &fstats) != -1) && fstats.st_size >= logs->maxsize)
 		{
-			if (logs->maxsize && (stat(logs->file, &fstats) != -1) && fstats.st_size >= logs->maxsize)
+			char oldlog[512];
+			if (logs->logfd == -1)
 			{
-				char oldlog[512];
-				if (logs->logfd == -1)
-				{
-					/* Try to open, so we can write the 'Max file size reached' message. */
-					logs->logfd = fd_fileopen(logs->file, O_CREAT|O_APPEND|O_WRONLY);
-				}
-				if (logs->logfd != -1)
-				{
-					if (write(logs->logfd, "Max file size reached, starting new log file\n", 45) < 0)
-					{
-						/* We already handle the unable to write to log file case for normal data.
-						 * I think we can get away with not handling this one.
-						 */
-						;
-					}
-					fd_close(logs->logfd);
-				}
-
-				/* Rename log file to xxxxxx.old */
-				snprintf(oldlog, sizeof(oldlog), "%s.old", logs->file);
-				rename(logs->file, oldlog);
-
-				logs->logfd = fd_fileopen(logs->file, O_CREAT|O_WRONLY|O_TRUNC);
-				if (logs->logfd == -1)
-					continue;
-			}
-			else if (logs->logfd == -1) {
+				/* Try to open, so we can write the 'Max file size reached' message. */
 				logs->logfd = fd_fileopen(logs->file, O_CREAT|O_APPEND|O_WRONLY);
-				if (logs->logfd == -1)
-				{
-					if (!loop.ircd_booted)
-					{
-						config_status("WARNING: Unable to write to '%s': %s", logs->file, strerror(ERRNO));
-					} else {
-						if (last_log_file_warning + 300 < TStime())
-						{
-							config_status("WARNING: Unable to write to '%s': %s. This warning will not re-appear for at least 5 minutes.", logs->file, strerror(ERRNO));
-							last_log_file_warning = TStime();
-						}
-					}
-					continue;
-				}
 			}
-			/* this shouldn't happen, but lets not waste unnecessary syscalls... */
+			if (logs->logfd != -1)
+			{
+				if (write(logs->logfd, "Max file size reached, starting new log file\n", 45) < 0)
+				{
+					/* We already handle the unable to write to log file case for normal data.
+					 * I think we can get away with not handling this one.
+					 */
+					;
+				}
+				fd_close(logs->logfd);
+			}
+
+			/* Rename log file to xxxxxx.old */
+			snprintf(oldlog, sizeof(oldlog), "%s.old", logs->file);
+			rename(logs->file, oldlog);
+
+			logs->logfd = fd_fileopen(logs->file, O_CREAT|O_WRONLY|O_TRUNC);
 			if (logs->logfd == -1)
 				continue;
-			if (write(logs->logfd, timebuf, strlen(timebuf)) < 0)
-			{
-				/* Let's ignore any write errors for this one. Next write() will catch it... */
-				;
-			}
-			n = write(logs->logfd, buf, strlen(buf));
-			if (n == strlen(buf))
-			{
-				written++;
-			}
-			else
+		}
+		else if (logs->logfd == -1) {
+			logs->logfd = fd_fileopen(logs->file, O_CREAT|O_APPEND|O_WRONLY);
+			if (logs->logfd == -1)
 			{
 				if (!loop.ircd_booted)
 				{
@@ -231,6 +206,33 @@ void ircd_log(int flags, FORMAT_STRING(const char *format), ...)
 						config_status("WARNING: Unable to write to '%s': %s. This warning will not re-appear for at least 5 minutes.", logs->file, strerror(ERRNO));
 						last_log_file_warning = TStime();
 					}
+				}
+				continue;
+			}
+		}
+		/* this shouldn't happen, but lets not waste unnecessary syscalls... */
+		if (logs->logfd == -1)
+			continue;
+		if (write(logs->logfd, timebuf, strlen(timebuf)) < 0)
+		{
+			/* Let's ignore any write errors for this one. Next write() will catch it... */
+			;
+		}
+		n = write(logs->logfd, buf, strlen(buf));
+		if (n == strlen(buf))
+		{
+			written++;
+		}
+		else
+		{
+			if (!loop.ircd_booted)
+			{
+				config_status("WARNING: Unable to write to '%s': %s", logs->file, strerror(ERRNO));
+			} else {
+				if (last_log_file_warning + 300 < TStime())
+				{
+					config_status("WARNING: Unable to write to '%s': %s. This warning will not re-appear for at least 5 minutes.", logs->file, strerror(ERRNO));
+					last_log_file_warning = TStime();
 				}
 			}
 		}
