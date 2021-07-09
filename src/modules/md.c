6ca3351ddaef337e8f2e4b76edc998ffb356ae0f
ValwareIRC <v.a.pond@outlook.com>
2021-07-09 23:55:16 +0100

    Update md.c

--- src/modules/md.c
+++ src/modules/md.c
@@ -106,7 +106,7 @@ CMD_FUNC(cmd_md)
 		if (!md || !md->unserialize || !target)
 			return;
 
-		if (MyConnect(target) && !md->remote_write && strcmp(md->name,"saslmechlist") != 0 && strcmp(client->name,target->name) != 0)
+		if (MyConnect(target) && !md->remote_write)
 		{
 			ircd_log(LOG_ERROR, "Remote server '%s' tried to write moddata '%s' of a client from ours '%s' -- attempt blocked.",
 			         client->name, md->name, target->name);
