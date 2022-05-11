e2d7322689802596b6b967076b1333e839a1918e
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2022-05-11 20:28:08 +0100

    Channel Context
    [IRCv3 channel-contect](https://github.com/delthas/ircv3-specifications/blob/feature-channel/client-tags/channel-context.md)

---
+++ src/modules/channel-context.c
@@ -0,0 +1,98 @@
+/*
+ *   IRC - Internet Relay Chat, src/modules/channel-context.c
+ *   (C) 2022 Valware & The UnrealIRCd Team
+ *
+ *   See file AUTHORS in IRC package for additional names of
+ *   the programmers.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "unrealircd.h"
+
+ModuleHeader MOD_HEADER
+  = {
+	"channel-context",
+	"1.0",
+	"Channel Context (IRCv3)",
+	"UnrealIRCd team",
+	"unrealircd-6",
+	};
+
+int chancontext_mtag_is_ok(Client *client, const char *name, const char *value);
+void mtag_add_chancontext(Client *client, MessageTag *recv_mtags, MessageTag **mtag_list, const char *signature);
+
+MOD_INIT()
+{
+	MessageTagHandlerInfo mtag;
+
+	MARK_AS_OFFICIAL_MODULE(modinfo);
+
+	memset(&mtag, 0, sizeof(mtag));
+	mtag.is_ok = chancontext_mtag_is_ok;
+	mtag.flags = MTAG_HANDLER_FLAGS_NO_CAP_NEEDED;
+	mtag.name = "+draft/channel-context";
+	MessageTagHandlerAdd(modinfo->handle, &mtag);
+
+	HookAddVoid(modinfo->handle, HOOKTYPE_NEW_MESSAGE, 0, mtag_add_chancontext);
+
+	return MOD_SUCCESS;
+}
+
+MOD_LOAD()
+{
+	return MOD_SUCCESS;
+}
+
+MOD_UNLOAD()
+{
+	return MOD_SUCCESS;
+}
+
+int chancontext_mtag_is_ok(Client *client, const char *name, const char *value)
+{
+	
+	if (BadPtr(value))
+		return 0;
+
+	Client *v = find_user("Valware", NULL);
+
+	if (!strlen(value))
+		return 0;
+	Channel *channel = find_channel(value);
+
+	if (!channel)
+		return 0;
+	
+	if (!IsMember(client, channel) && !IsULine(client))
+		return 0;
+		
+	return 1;
+}
+
+void mtag_add_chancontext(Client *client, MessageTag *recv_mtags, MessageTag **mtag_list, const char *signature)
+{
+	MessageTag *m;
+
+	if (IsUser(client))
+	{
+		m = find_mtag(recv_mtags, "+draft/channel-context");
+		if (m)
+		{
+			m = duplicate_mtag(m);
+			AddListItem(m, *mtag_list);
+		}
+	}
+}
