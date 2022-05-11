cd48fec82656383ae3302a8d7e4471b954a42bbe
Bram Matthys <syzop@vulnscan.org>
2022-05-11 09:21:32 +0200

    The /LICENSE command confusingly said that the UnrealIRCd license is GPLv1 or later. This updates the include/license.h file, which is used for the /LICENSE command, to say "GPLv2 or later".
    The 'LICENSE' file shipped with UnrealIRCd since at least the year 2000
    has always been the GPLv2.
    
    In the copyright headers of individual .c and .h files we have a mix of
    "GPLv1 or later" and "GPLv2 or later", so "GPLv2 or later" is the common
    denominator.

--- include/license.h
+++ include/license.h
@@ -27,10 +27,10 @@
 
 char *gnulicense[] = {
 	"   \2UnrealIRCd License\2",
-	"This program is free software; you can redistribute it and/or modify",
-	"it under the terms of the GNU General Public License as published by",
-	"the Free Software Foundation; either version 1, or (at your option)",
-	"any later version.",
+	"This program is free software; you can redistribute it and/or",
+	"modify it under the terms of the GNU General Public License",
+	"as published by the Free Software Foundation; either version 2",
+	"of the License, or (at your option) any later version.",
 	"",
 	"This program is distributed in the hope that it will be useful,",
 	"but WITHOUT ANY WARRANTY; without even the implied warranty of",
@@ -39,10 +39,11 @@ char *gnulicense[] = {
 	"",
 	"You should have received a copy of the GNU General Public License",
 	"along with this program; if not, write to the Free Software",
-	"Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.",
+	"Foundation, Inc., 51 Franklin Street, Fifth Floor,",
+	"Boston, MA  02110-1301, USA.",
 	"",
 	"To see the UnrealIRCd License, please point your browser",
-	"at http://www.gnu.org/copyleft/gpl.html or look in the",
-	"file LICENSE in the UnrealIRCd dist",
-	0
+	"to https://www.gnu.org/licenses/old-licenses/gpl-2.0.html",
+	"or look at the LICENSE file in the UnrealIRCd distribution.",
+	NULL
 };
