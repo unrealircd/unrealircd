6b30482c0461d58d75bfd04b98ce27b400e3d6a0
Bram Matthys <syzop@vulnscan.org>
2022-06-08 09:03:59 +0200

    Don't apply registration timeout to *NIX domain socket connections. May want to reconsider this but.. for now..

--- src/ircd.c
+++ src/ircd.c
@@ -121,7 +121,9 @@ EVENT(handshake_timeout)
 
 	list_for_each_entry_safe(client, next, &unknown_list, lclient_node)
 	{
-		if (client->local->creationtime && ((TStime() - client->local->creationtime) > iConf.handshake_timeout))
+		if (client->local->creationtime &&
+		    ((TStime() - client->local->creationtime) > iConf.handshake_timeout) &&
+		    !(client->local->listener && (client->local->listener->socket_type == SOCKET_TYPE_UNIX)))
 		{
 			if (client->server && *client->server->by)
 				continue; /* handled by server module */
