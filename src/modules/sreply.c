3804cba3e1431abf88b8aa5a7044170b13fb1c33
Valerie Liu <79415174+ValwareIRC@users.noreply.github.com>
2023-09-23 09:30:43 +0100

    sreply.c: Fix accidental truncation
    This was causing the rest of the message to be cut off, where it should all be contained in `parv[3]`

--- src/modules/sreply.c
+++ src/modules/sreply.c
@@ -36,7 +36,7 @@ ModuleHeader MOD_HEADER
 /* This is called on module init, before Server Ready */
 MOD_INIT()
 {
-	CommandAdd(modinfo->handle, "SREPLY", cmd_sreply, MAXPARA, CMD_SERVER);
+	CommandAdd(modinfo->handle, "SREPLY", cmd_sreply, 4, CMD_SERVER);
 	MARK_AS_OFFICIAL_MODULE(modinfo);
 	return MOD_SUCCESS;
 }
