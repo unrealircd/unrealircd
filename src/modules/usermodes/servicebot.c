c028d738d2a02b582a06237986a18dedaf93bda9
Valerie Liu <79415174+ValwareIRC@users.noreply.github.com>
2023-12-26 00:41:45 +0000

    Update servicebot.c

--- src/modules/usermodes/servicebot.c
+++ src/modules/usermodes/servicebot.c
@@ -1,4 +1,5 @@
-/*
+/* 
+ * IRC - Internet Relay Chat, src/modules/usermodes/servicebot.c
  * Prevents you from being kicked (User mode +q)
  * (C) Copyright 2000-.. Bram Matthys (Syzop) and the UnrealIRCd team
  *
