c8a1b82f6f9620cc8c0d01176d4247396b989d51
Valerie Liu <79415174+ValwareIRC@users.noreply.github.com>
2023-12-26 00:39:11 +0000

    Update noctcp.c

--- src/modules/usermodes/noctcp.c
+++ src/modules/usermodes/noctcp.c
@@ -1,4 +1,5 @@
-/*
+/* 
+ * IRC - Internet Relay Chat, src/modules/usermodes/noctcp.c
  * Block user-to-user CTCP UnrealIRCd Module (User Mode +T)
  * (C) Copyright 2000-.. Bram Matthys (Syzop) and the UnrealIRCd team
  *
