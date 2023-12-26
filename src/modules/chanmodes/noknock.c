b2d4ee2fd5825f6d8bc02df6dc2c744730ca9f15
Valerie Liu <79415174+ValwareIRC@users.noreply.github.com>
2023-12-26 01:00:33 +0000

    Update noknock.c

--- src/modules/chanmodes/noknock.c
+++ src/modules/chanmodes/noknock.c
@@ -1,4 +1,5 @@
-/*
+/* 
+ * IRC - Internet Relay Chat, src/modules/chanmodes/noknock.c
  * Disallow knocks UnrealIRCd Module (Channel Mode +K)
  * (C) Copyright 2014 Travis McArthur (Heero) and the UnrealIRCd team
  *
