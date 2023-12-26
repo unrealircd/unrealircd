c7d9deb074d8aeb53659d59943e9eee0634a3826
Valerie Liu <79415174+ValwareIRC@users.noreply.github.com>
2023-12-26 00:42:01 +0000

    Update showwhois.c

--- src/modules/usermodes/showwhois.c
+++ src/modules/usermodes/showwhois.c
@@ -1,4 +1,5 @@
-/*
+/* 
+ * IRC - Internet Relay Chat, src/modules/usermodes/showwhois.c
  * Show when someone does a /WHOIS on you (User mode +W)
  * (C) Copyright 2000-.. Bram Matthys (Syzop) and the UnrealIRCd team
  *
