bcb40a6351f7e4a46fd7e6759e6cb5070474c9ca
Valerie Liu <79415174+ValwareIRC@users.noreply.github.com>
2023-12-26 00:40:51 +0000

    Update regonlymsg.c

--- src/modules/usermodes/regonlymsg.c
+++ src/modules/usermodes/regonlymsg.c
@@ -1,4 +1,5 @@
-/*
+/* 
+ * IRC - Internet Relay Chat, src/modules/usermodes/regonlymsg.c
  * Recieve private messages only from registered users (User mode +R)
  * (C) Copyright 2000-.. Bram Matthys (Syzop) and the UnrealIRCd team
  *
