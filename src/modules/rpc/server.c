299be35f8224322df4e85cb6e0ea0f834dd73897
Valerie Liu <79415174+ValwareIRC@users.noreply.github.com>
2023-12-26 00:46:09 +0000

    Update server.c

--- src/modules/rpc/server.c
+++ src/modules/rpc/server.c
@@ -1,4 +1,6 @@
-/* server.* RPC calls
+/*  
+ * IRC - Internet Relay Chat, src/modules/rpc/server.c
+ * server.* RPC calls
  * (C) Copyright 2022-.. Bram Matthys (Syzop) and the UnrealIRCd team
  * License: GPLv2 or later
  */
