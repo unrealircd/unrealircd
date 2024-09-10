e0459943a3045afd5356e422fd06259f9ad6e6e4
Valerie Liu <79415174+ValwareIRC@users.noreply.github.com>
2024-09-10 22:40:33 +0800

    Show port number in DEBUG_TLS_FATAL_ERROR (#298)

--- src/tls.c
+++ src/tls.c
@@ -917,10 +917,12 @@ static int fatal_tls_error(int ssl_error, int where, int my_errno, Client *clien
 
 	SetDeadSocket(client);
 	unreal_log(ULOG_DEBUG, "tls", "DEBUG_TLS_FATAL_ERROR", client,
-		   "Exiting TLS client $client.details: $tls_function: $tls_error_string: $tls_additional_info",
+		   "Exiting TLS client $client.details [port $port]: $tls_function: $tls_error_string: $tls_additional_info",
 		   log_data_string("tls_function", ssl_func),
 		   log_data_string("tls_error_string", ssl_errstr),
-		   log_data_string("tls_additional_info", additional_info));
+		   log_data_string("tls_additional_info", additional_info),
+		   log_data_integer("port", client->local->listener ? client->local->listener->port : 0));
+
 
 	if (where == FUNC_TLS_CONNECT)
 	{
