f084ac6c50e040b2c737b89564d3a036b53cc433
Valerie Liu <79415174+ValwareIRC@users.noreply.github.com>
2025-04-29 23:22:44 +0100

    Remove weird thing that I didn't put in there o.o

--- src/modules/nick.c
+++ src/modules/nick.c
@@ -853,9 +853,6 @@ void welcome_user(Client *client, TKL *viruschan_tkl)
 
 	if (client->umodes & UMODE_INVISIBLE)
 		irccounts.invisible++;
-
-	/* set::modes-on-connect */
-	client->umodes |= get_setting_for_user_number(client, SET_MODES_ON_CONNECT);
 	
 	build_umode_string(client, 0, SEND_UMODES|UMODE_SERVNOTICE, buf);
 
