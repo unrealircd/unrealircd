unrealircd/unrealircd

commit 31d51fbb04f5f2659cea4bf348d91019d574467c

* UnrealIRCd can now be used if your OpenSSL does not provide MD5   (there will be an error if you use `cloak_md5`, but everything   will work fine if you use `cloak_sha256`).
We phased out MD5 usage years ago, so it is only contained to
the old cloaking module. In fact that was the only reason we
started to provide the SHA256 cloaking module, simply so it
isn't using old MD5.

Of course, for module coders this means they should not call
DoMD5() or md5hash(), but that would be rare. Currently zero
modules in unrealircd contrib do this and it makes no sense
to start using it nowadays anyway.

src/modules/cloak_md5.c
@@ -70,6 +70,16 @@ MOD_TEST()
 	}
 	HookAdd(modinfo->handle, HOOKTYPE_CONFIGTEST, 0, cloak_config_test);
 	HookAdd(modinfo->handle, HOOKTYPE_CONFIGPOSTTEST, 0, cloak_config_posttest);
+
+	if (!md5_function)
+	{
+		unreal_log(ULOG_ERROR, "tls", "MD5_UNAVAILABLE", NULL,
+		           "You are trying to load the 'cloak_md5' module but the MD5 "
+		           "algorithm is not available in your OpenSSL/LibreSSL version. "
+		           "Either switch to 'cloak_sha256' or change your SSL library "
+		           "configuration (consult your distro docs).");
+		return MOD_FAILED;
+	}
 	return MOD_SUCCESS;
 }
 

src/misc.c
@@ -1863,7 +1863,17 @@ void DoMD5(char *mdout, const char *src, unsigned long n)
 {
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
 	unsigned int md_len;
-	EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+	EVP_MD_CTX *mdctx;
+
+	if (!md5_function)
+	{
+		unreal_log(ULOG_FATAL, "tls", "MD5_UNAVAILABLE_FATAL", NULL,
+		           "DoMD5() was called but the MD5 algorithm is not available "
+		           "in your OpenSSL/LibreSSL version. -- ABORTING");
+		abort();
+	}
+
+	mdctx = EVP_MD_CTX_new();
 	if (EVP_DigestInit_ex(mdctx, md5_function, NULL) != 1)
 		abort();
 	EVP_DigestUpdate(mdctx, src, n);

src/tls.c
@@ -606,11 +606,6 @@ int early_init_tls(void)
 	}
 
 	md5_function = EVP_MD_fetch(NULL, "MD5", NULL);
-	if (!md5_function)
-	{
-		fprintf(stderr, "Could not find MD5 algorithm in TLS library\n");
-		exit(6);
-	}
 #endif
 	return 1;
 }

commit 8a4dae71fbedb0046c28ec5012204679aa27f520

Fix compile problem with LibreSSL (and possibly OpenSSL <3.0.0). Caused by 31d51fbb04f5f2659cea4bf348d91019d574467c

src/modules/cloak_md5.c
@@ -71,6 +71,7 @@ MOD_TEST()
 	HookAdd(modinfo->handle, HOOKTYPE_CONFIGTEST, 0, cloak_config_test);
 	HookAdd(modinfo->handle, HOOKTYPE_CONFIGPOSTTEST, 0, cloak_config_posttest);
 
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
 	if (!md5_function)
 	{
 		unreal_log(ULOG_ERROR, "tls", "MD5_UNAVAILABLE", NULL,
@@ -80,6 +81,7 @@ MOD_TEST()
 		           "configuration (consult your distro docs).");
 		return MOD_FAILED;
 	}
+#endif
 	return MOD_SUCCESS;
 }
 