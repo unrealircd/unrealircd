e3b92cc084ed82b51e54a76f7af769204c176738
Bram Matthys <syzop@vulnscan.org>
2025-09-28 16:20:19 +0200

    away_join() optimization: don't bother if user is not away. Otherwise we are iterating <num channel members> all for nothing.

--- src/modules/away.c
+++ src/modules/away.c
@@ -68,7 +68,13 @@ int away_join(Client *client, Channel *channel, MessageTag *mtags)
 {
 	Member *lp;
 	Client *acptr;
-	int invisible = invisible_user_in_channel(client, channel);
+	int invisible;
+
+	if (!client->user->away)
+		return 0; /* Nothing to do */
+
+	invisible = invisible_user_in_channel(client, channel);
+
 	for (lp = channel->members; lp; lp = lp->next)
 	{
 		acptr = lp->client;
@@ -79,7 +85,7 @@ int away_join(Client *client, Channel *channel, MessageTag *mtags)
 		if (invisible && !check_channel_access_member(lp, "hoaq") && (client != acptr))
 			continue; /* skip non-ops if requested to (used for mode +D), but always send to 'client' */
 
-		if (client->user->away && HasCapabilityFast(acptr, CAP_AWAY_NOTIFY))
+		if (HasCapabilityFast(acptr, CAP_AWAY_NOTIFY))
 		{
 			MessageTag *mtags_away = NULL;
 			new_message(client, NULL, &mtags_away);
