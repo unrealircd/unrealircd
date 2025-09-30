unrealircd/unrealircd

commit 15c8b1aa40be2cc65db292ccffd455303f51af07

Optimization: Use HasCapabilityFast() in whox.c and extended-monitor.c The whox one saves a lookup for each channel member (so eg 500 for a channel with 500 members). The extended-monitor saves it on delivering watch/monitor notifications, so depends on the # of subscriptions. And that's each time such a command is called. We now only lookup on MOD_LOAD.

src/modules/whox.c
@@ -77,6 +77,9 @@ struct who_format
 /* Global variables */
 ModDataInfo *whox_md = NULL;
 
+/* Externally looked up */
+long CAP_MULTI_PREFIX = 0;
+
 /* Forward declarations */
 CMD_FUNC(cmd_whox);
 static void who_global(Client *client, char *mask, int operspy, struct who_format *fmt);
@@ -121,6 +124,7 @@ MOD_INIT()
 
 MOD_LOAD()
 {
+	long CAP_MULTI_PREFIX = ClientCapabilityBit("multi-prefix");
 	return MOD_SUCCESS;
 }
 
@@ -741,7 +745,7 @@ static void do_who(Client *client, Client *acptr, Channel *channel, struct who_f
 
 		if ((lp = find_membership_link(acptr->user->channel, channel)))
 		{
-			if (!(fmt->fields || HasCapability(client, "multi-prefix")))
+			if (!(fmt->fields || HasCapabilityFast(client, CAP_MULTI_PREFIX)))
 			{
 				/* Standard NAMES reply (single character) */
 				char c = mode_to_prefix(*lp->member_modes);

src/modules/extended-monitor.c
@@ -22,8 +22,16 @@
 
 #include "unrealircd.h"
 
+/* Global variables */
 long CAP_EXTENDED_MONITOR = 0L;
 
+/* externally looked up: */
+long CAP_AWAY_NOTIFY = 0;
+long CAP_ACCOUNT_NOTIFY = 0;
+long CAP_CHGHOST = 0;
+long CAP_SETNAME = 0;
+
+/* Forward declarations */
 int extended_monitor_away(Client *client, MessageTag *mtags, const char *reason, int already_as_away);
 int extended_monitor_account_login(Client *client, MessageTag *mtags);
 int extended_monitor_userhost_change(Client *client, const char *olduser, const char *oldhost);
@@ -67,6 +75,10 @@ MOD_INIT()
 
 MOD_LOAD()
 {
+	CAP_AWAY_NOTIFY = ClientCapabilityBit("away-notify");
+	CAP_ACCOUNT_NOTIFY = ClientCapabilityBit("account-notify");
+	CAP_CHGHOST = ClientCapabilityBit("chghost");
+	CAP_SETNAME = ClientCapabilityBit("setname");
 	return MOD_SUCCESS;
 }
 
@@ -121,27 +133,27 @@ int extended_monitor_notification(Client *client, Watch *watch, Link *lp, int ev
 	switch (event)
 	{
 		case WATCH_EVENT_AWAY:
-			if (HasCapability(lp->value.client, "away-notify"))
+			if (HasCapabilityFast(lp->value.client, CAP_AWAY_NOTIFY))
 				sendto_prefix_one(lp->value.client, client, NULL, ":%s AWAY :%s", client->name, client->user->away);
 			break;
 		case WATCH_EVENT_NOTAWAY:
-			if (HasCapability(lp->value.client, "away-notify"))
+			if (HasCapabilityFast(lp->value.client, CAP_AWAY_NOTIFY))
 				sendto_prefix_one(lp->value.client, client, NULL, ":%s AWAY", client->name);
 			break;
 		case WATCH_EVENT_LOGGEDIN:
-			if (HasCapability(lp->value.client, "account-notify"))
+			if (HasCapabilityFast(lp->value.client, CAP_ACCOUNT_NOTIFY))
 				sendto_prefix_one(lp->value.client, client, NULL, ":%s ACCOUNT :%s", client->name, client->user->account);
 			break;
 		case WATCH_EVENT_LOGGEDOUT:
-			if (HasCapability(lp->value.client, "account-notify"))
+			if (HasCapabilityFast(lp->value.client, CAP_ACCOUNT_NOTIFY))
 				sendto_prefix_one(lp->value.client, client, NULL, ":%s ACCOUNT :*", client->name);
 			break;
 		case WATCH_EVENT_USERHOST:
-			if (HasCapability(lp->value.client, "chghost"))
+			if (HasCapabilityFast(lp->value.client, CAP_CHGHOST))
 				sendto_prefix_one(lp->value.client, client, NULL, ":%s CHGHOST %s %s", client->name, client->user->username, GetHost(client));
 			break;
 		case WATCH_EVENT_REALNAME:
-			if (HasCapability(lp->value.client, "setname"))
+			if (HasCapabilityFast(lp->value.client, CAP_SETNAME))
 				sendto_prefix_one(lp->value.client, client, NULL, ":%s SETNAME :%s", client->name, client->info);
 			break;
 		default:

commit e9000f91487313f2e943c97c057c2a736d8b179c

Fix multi-prefix
Broken by 15c8b1aa40be2cc65db292ccffd455303f51af07

src/modules/whox.c
@@ -124,7 +124,7 @@ MOD_INIT()
 
 MOD_LOAD()
 {
-	long CAP_MULTI_PREFIX = ClientCapabilityBit("multi-prefix");
+	CAP_MULTI_PREFIX = ClientCapabilityBit("multi-prefix");
 	return MOD_SUCCESS;
 }
 