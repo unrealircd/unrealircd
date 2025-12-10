unrealircd/unrealircd

commit 877d151da41f3a20fa277f0a919c69576a4611ac

Support multiple TLS certificates/keys, e.g. ECDSA + ML-DSA (PQC).
In the past a dual cert/key setup could have been useful for RSA + ECDSA
but nowadays all clients support ECDSA so that makes little sense.
The reason it is added now is so you can use ECDSA + ML-DSA or some
other [regular crypto] + [post quantum crypto] combination.
Actually, you could even use more than two.

To use this in the config file, simply use the certificate and key
directive multiple times. Just be sure to load the certificates and keys
in the same order. We will print a helpful error if you fail to do so.

Note that for Post Quantum Cryptography the most important step today
was/is to protect against the "Harvest now, decrypt later" scenario
https://en.wikipedia.org/wiki/Harvest_now,_decrypt_later which is a
"passive attack". That's why in UnrealIRCd 6.2.0 we enabled
X25519MLKEM768 if it is available (OpenSSL 3.5.0 and later).
While, this commit, and this talk about dual ECDSA and ML-DSA, is about
when a quantum computer exists and actively does a man in the middle
attack. That's not a realistic scenario in 2025 and according to experts
also not in the next few years. We just make the UnrealIRCd code-
base ready to have this feature for when it is needed / will be used,
and to get this tested properly.

For testing the dual ECDSA and ML-DSA setup I used the following
command to create the 2nd cert/key (self-signed):

openssl req -x509 -nodes -newkey mldsa65 \
  -keyout ~/unrealircd/conf/tls/server.key.mdsa65.pem \
  -out ~/unrealircd/conf/tls/server.cert.mdsa65.pem \
  -days 3650

And then:

listen {
        ip *;
        port 6697;
        options { tls; }
        tls-options {
                certificate "ssl/server.cert.pem";
                key "ssl/server.key.pem";
                certificate "ssl/server.cert.mdsa65.pem";
                key "ssl/server.key.mdsa65.pem";
        }
}

When running openssl s_client -connect 127.0.0.1:6697 it shows ML-DSA is used:
...

src/conf.c
@@ -1795,9 +1795,9 @@ void config_setdefaultsettings(Configuration *i)
 	/* TLS options */
 	i->tls_options = safe_alloc(sizeof(TLSOptions));
 	snprintf(tmp, sizeof(tmp), "%s/tls/server.cert.pem", CONFDIR);
-	safe_strdup(i->tls_options->certificate_file, tmp);
+	add_name_list(i->tls_options->certificate_files, tmp);
 	snprintf(tmp, sizeof(tmp), "%s/tls/server.key.pem", CONFDIR);
-	safe_strdup(i->tls_options->key_file, tmp);
+	add_name_list(i->tls_options->key_files, tmp);
 	snprintf(tmp, sizeof(tmp), "%s/tls/curl-ca-bundle.crt", CONFDIR);
 	safe_strdup(i->tls_options->trusted_ca_file, tmp);
 	safe_strdup(i->tls_options->ciphers, UNREALIRCD_DEFAULT_CIPHERS);
@@ -7279,6 +7279,7 @@ void test_tlsblock(ConfigFile *conf, ConfigEntry *cep, int *totalerrors)
 {
 	ConfigEntry *cepp, *ceppp;
 	int errors = 0;
+	int tls_keys = 0, tls_certificates = 0;
 
 	for (cepp = cep->items; cepp; cepp = cepp->next)
 	{
@@ -7373,6 +7374,10 @@ void test_tlsblock(ConfigFile *conf, ConfigEntry *cep, int *totalerrors)
 		{
 			char *path;
 			CheckNull(cepp);
+			if (!strcmp(cepp->name, "key"))
+				tls_keys++;
+			if (!strcmp(cepp->name, "certificate"))
+				tls_certificates++;
 			path = convert_to_absolute_path_duplicate(cepp->value, CONFDIR);
 			if (!file_exists(path))
 			{
@@ -7510,6 +7515,15 @@ void test_tlsblock(ConfigFile *conf, ConfigEntry *cep, int *totalerrors)
 		}
 	}
 
+	if (tls_keys != tls_certificates)
+	{
+		config_error("%s:%d: certificate count != key count. "
+		             "Each certificate should have a matching key. You cannot have more "
+		             "certificates than keys or the other way around.",
+		             cep->file->filename, cep->line_number);
+		errors++;
+	}
+
 	*totalerrors += errors;
 }
 
@@ -7518,8 +7532,8 @@ void free_tls_options(TLSOptions *tlsoptions)
 	if (!tlsoptions)
 		return;
 
-	safe_free(tlsoptions->certificate_file);
-	safe_free(tlsoptions->key_file);
+	safe_free_name_list(tlsoptions->certificate_files);
+	safe_free_name_list(tlsoptions->key_files);
 	safe_free(tlsoptions->trusted_ca_file);
 	safe_free(tlsoptions->ciphers);
 	safe_free(tlsoptions->ciphersuites);
@@ -7538,8 +7552,8 @@ void conf_tlsblock(ConfigFile *conf, ConfigEntry *cep, TLSOptions *tlsoptions)
 	/* First, inherit settings from set::options::tls */
 	if (tlsoptions != tempiConf.tls_options)
 	{
-		safe_strdup(tlsoptions->certificate_file, tempiConf.tls_options->certificate_file);
-		safe_strdup(tlsoptions->key_file, tempiConf.tls_options->key_file);
+		// certificate_files: done at end of function
+		// key_files: done at end of function
 		safe_strdup(tlsoptions->trusted_ca_file, tempiConf.tls_options->trusted_ca_file);
 		tlsoptions->protocols = tempiConf.tls_options->protocols;
 		safe_strdup(tlsoptions->ciphers, tempiConf.tls_options->ciphers);
@@ -7615,12 +7629,12 @@ void conf_tlsblock(ConfigFile *conf, ConfigEntry *cep, TLSOptions *tlsoptions)
 		else if (!strcmp(cepp->name, "certificate"))
 		{
 			convert_to_absolute_path(&cepp->value, CONFDIR);
-			safe_strdup(tlsoptions->certificate_file, cepp->value);
+			add_name_list(tlsoptions->certificate_files, cepp->value);
 		}
 		else if (!strcmp(cepp->name, "key"))
 		{
 			convert_to_absolute_path(&cepp->value, CONFDIR);
-			safe_strdup(tlsoptions->key_file, cepp->value);
+			add_name_list(tlsoptions->key_files, cepp->value);
 		}
 		else if (!strcmp(cepp->name, "trusted-ca-file"))
 		{
@@ -7672,6 +7686,21 @@ void conf_tlsblock(ConfigFile *conf, ConfigEntry *cep, TLSOptions *tlsoptions)
 		{
 			tlsoptions->certificate_expiry_notification = config_checkval(cepp->value, CFG_YESNO);
 		}
+	}
+
+	/* Inheritance of these items is at the end of this function and
+	 * only if they were not set in the block. The reason for that is
+	 * that if there is a 'certificate' and 'key' in a tls options
+	 * config blob, it should override, while otherwise it would 'add'
+	 * additional certs/keys due to the nature of it being a name list.
+	 * So we simply only add these here at the end if they were not set.
+	 */
+	if (tlsoptions != tempiConf.tls_options)
+	{
+		if (!tlsoptions->certificate_files)
+			tlsoptions->certificate_files = duplicate_name_list(tempiConf.tls_options->certificate_files);
+		if (!tlsoptions->key_files)
+			tlsoptions->key_files = duplicate_name_list(tempiConf.tls_options->key_files);
 	}
 }
 

include/struct.h
@@ -802,6 +802,8 @@ struct NameList {
 
 /** Free an entire NameList */
 #define free_entire_name_list(list) do { _free_entire_name_list(list); list = NULL; } while(0)
+#define safe_free_name_list free_entire_name_list
+
 /** Add an entry to a NameList */
 #define add_name_list(list, str)  _add_name_list(&list, str)
 /** Delete an entry from a NameList - AND free it */
@@ -1851,8 +1853,8 @@ struct ConfigItem_oper {
  */
 typedef struct TLSOptions TLSOptions;
 struct TLSOptions {
-	char *certificate_file;
-	char *key_file;
+	NameList *certificate_files;
+	NameList *key_files;
 	char *trusted_ca_file;
 	unsigned int protocols;
 	char *ciphers;

src/modules/stats.c
@@ -549,19 +549,31 @@ int stats_port(Client *client, const char *para)
 			continue;
 		if (listener->socket_type == SOCKET_TYPE_UNIX)
 		{
-			sendnotice(client, "*** Listener on %s (UNIX): has %i client(s), options: %s %s",
+			sendtxtnumeric(client, "Listener on %s (UNIX): has %i client(s), options: %s %s",
 				   listener->file,
 				   listener->clients,
 				   stats_port_helper(listener),
 				   listener->flag.temporary ? "[TEMPORARY]" : "");
 		} else {
-			sendnotice(client, "*** Listener on %s:%i (%s): has %i client(s), options: %s %s",
+			sendtxtnumeric(client, "Listener on %s:%i (%s): has %i client(s), options: %s %s",
 				   listener->ip,
 				   listener->port,
 				   listener->socket_type == SOCKET_TYPE_IPV6 ? "IPv6" : "IPv4",
 				   listener->clients,
 				   stats_port_helper(listener),
 				   listener->flag.temporary ? "[TEMPORARY]" : "");
+		}
+		if (listener->options & LISTENER_TLS)
+		{
+			NameList *n, *n2;
+			if (listener->tls_options)
+			{
+				for (n = listener->tls_options->certificate_files, n2 = listener->tls_options->key_files; n && n2; n = n->next, n2 = n2->next)
+					sendtxtnumeric(client, "- using tls certificate %s + key %s", n->name, n2->name);
+			} else {
+				for (n = iConf.tls_options->certificate_files, n2 = iConf.tls_options->key_files; n && n2; n = n->next, n2 = n2->next)
+					sendtxtnumeric(client, "- using default tls certificate %s + key %s", n->name, n2->name);
+			}
 		}
 	}
 	return 0;
@@ -748,6 +760,7 @@ int stats_set(Client *client, const char *para)
 	char *uhallow;
 	SecurityGroup *s;
 	FloodSettings *f;
+	NameList *n;
 	char modebuf[BUFSIZE], parabuf[BUFSIZE];
 
 	if (!ValidatePermissionsForPath("server:info:stats",client,NULL,NULL,NULL))
@@ -818,8 +831,10 @@ int stats_set(Client *client, const char *para)
 	sendtxtnumeric(client, "hide-ban-reason: %d", HIDE_BAN_REASON);
 	sendtxtnumeric(client, "anti-spam-quit-message-time: %s", pretty_time_val(ANTI_SPAM_QUIT_MSG_TIME));
 	sendtxtnumeric(client, "channel-command-prefix: %s", CHANCMDPFX ? CHANCMDPFX : "`");
-	sendtxtnumeric(client, "tls::certificate: %s", SafePrint(iConf.tls_options->certificate_file));
-	sendtxtnumeric(client, "tls::key: %s", SafePrint(iConf.tls_options->key_file));
+	for (n = iConf.tls_options->certificate_files; n; n = n->next)
+		sendtxtnumeric(client, "tls::certificate: %s", n->name);
+	for (n = iConf.tls_options->key_files; n; n = n->next)
+		sendtxtnumeric(client, "tls::key: %s", n->name);
 	sendtxtnumeric(client, "tls::trusted-ca-file: %s", SafePrint(iConf.tls_options->trusted_ca_file));
 	sendtxtnumeric(client, "tls::options: %s", iConf.tls_options->options & TLSFLAG_FAILIFNOCERT ? "FAILIFNOCERT" : "");
 	sendtxtnumeric(client, "options::show-opermotd: %d", SHOWOPERMOTD);

src/tls.c
@@ -294,6 +294,7 @@ int unrealircd_set_tls_groups(SSL_CTX *ctx, const char *groups)
 SSL_CTX *init_ctx(TLSOptions *tlsoptions, int server)
 {
 	SSL_CTX *ctx;
+	NameList *n, *n2;
 	char *errstr = NULL;
 
 	if (server)
@@ -332,60 +333,84 @@ SSL_CTX *init_ctx(TLSOptions *tlsoptions, int server)
 	 * SSL_CTX_use_certificate_chain_file() later on works but produces like
 	 * four lines of output, which is a bit verbose for such a simple case.
 	 */
-	if (!file_exists(tlsoptions->certificate_file))
+	for (n = tlsoptions->certificate_files; n; n = n->next)
 	{
-		int saved_errno = errno;
-		unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
-		           "Could not open TLS certificate $filename: $system_error",
-		           log_data_string("filename", tlsoptions->certificate_file),
-		           log_data_string("system_error", strerror(saved_errno)));
-
-		if (str_ends_with_case_sensitive(tlsoptions->certificate_file, "tls/server.cert.pem"))
+		if (!file_exists(n->name))
 		{
-			unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED_DEFAULT_CERT", NULL,
-			           "It seems the default certificate is missing. "
-			           "Run 'make pem && make install' in the UnrealIRCd source directory "
-			           "to generate a self-signed cert.");
+			int saved_errno = errno;
+			unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
+				   "Could not open TLS certificate $filename: $system_error",
+				   log_data_string("filename", n->name),
+				   log_data_string("system_error", strerror(saved_errno)));
+
+			if (str_ends_with_case_sensitive(n->name, "tls/server.cert.pem"))
+			{
+				unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED_DEFAULT_CERT", NULL,
+					   "It seems the default certificate is missing. "
+					   "Run 'make pem && make install' in the UnrealIRCd source directory "
+					   "to generate a self-signed cert.");
+			}
+			goto fail;
 		}
-		goto fail;
 	}
 
-	if (SSL_CTX_use_certificate_chain_file(ctx, tlsoptions->certificate_file) <= 0)
+	/* And the same 'first check' for key the key file(s) */
+	for (n = tlsoptions->key_files; n; n = n->next)
 	{
-		unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
-		           "Failed to load TLS certificate $filename\n$tls_error.all",
-		           log_data_string("filename", tlsoptions->certificate_file),
-		           log_data_tls_error());
-		goto fail;
+		if (!file_exists(n->name))
+		{
+			int saved_errno = errno;
+			unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
+				   "Could not open TLS key $filename: $system_error",
+				   log_data_string("filename", n->name),
+				   log_data_string("system_error", strerror(saved_errno)));
+			goto fail;
+		}
 	}
 
-	/* Let's first check the simple case of file exist - this time for key file. */
-	if (!file_exists(tlsoptions->key_file))
-	{
-		int saved_errno = errno;
-		unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
-		           "Could not open TLS key $filename: $system_error",
-		           log_data_string("filename", tlsoptions->key_file),
-		           log_data_string("system_error", strerror(saved_errno)));
-		goto fail;
-	}
+	// TODO: verify same amount of certificate_files vs key_files :D
 
-	if (SSL_CTX_use_PrivateKey_file(ctx, tlsoptions->key_file, SSL_FILETYPE_PEM) <= 0)
+	for (n = tlsoptions->certificate_files, n2 = tlsoptions->key_files; n && n2; n = n->next, n2 = n2->next)
 	{
-		unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
-		           "Failed to load TLS private key $filename\n$tls_error.all",
-		           log_data_string("filename", tlsoptions->key_file),
-		           log_data_tls_error());
-		goto fail;
-	}
-
-	if (!SSL_CTX_check_private_key(ctx))
-	{
-		unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
-		           "Check for TLS private key failed $filename\n$tls_error.all",
-		           log_data_string("filename", tlsoptions->key_file),
-		           log_data_tls_error());
-		goto fail;
+		if (SSL_CTX_use_certificate_chain_file(ctx, n->name) <= 0)
+		{
+			unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
+				   "Failed to load TLS certificate $filename\n$tls_error.all",
+				   log_data_string("filename", n->name),
+				   log_data_tls_error());
+			goto fail;
+		}
+		if (SSL_CTX_use_PrivateKey_file(ctx, n2->name, SSL_FILETYPE_PEM) <= 0)
+		{
+			unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
+				   "Failed to load TLS private key $filename\n$tls_error.all",
+				   log_data_string("filename", n2->name),
+				   log_data_tls_error());
+			goto fail;
+		}
+		if (!SSL_CTX_check_private_key(ctx))
+		{
+			unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
+				   "Check for TLS private key(s) failed: "
+				   "certificate $certificate_filename vs key $key_filename\n"
+				   "$tls_error.all",
+				   log_data_string("certificate_filename", n->name),
+				   log_data_string("key_filename", n2->name),
+				   log_data_tls_error());
+			/* An extra hint for dual cert as this mistake will likely happen
+			 * to some users and the OpenSSL error may be a bit too cryptic.
+			 */
+			if (tlsoptions->certificate_files->next)
+			{
+				unreal_log(ULOG_ERROR, "config", "TLS_LOAD_FAILED", NULL,
+					   "HINT: You are using multiple 'certificate' and 'key' items. "
+					   "Make sure each certificate/key pair belongs to each other, "
+					   "they should be in the correct order! "
+					   "E.g. certificate \"cert1\"; key \"key1\"; certificate \"cert2\"; key \"key2\";"
+					   );
+			}
+			goto fail;
+		}
 	}
 
 	if (SSL_CTX_set_cipher_list(ctx, tlsoptions->ciphers) == 0)
@@ -420,10 +445,9 @@ SSL_CTX *init_ctx(TLSOptions *tlsoptions, int server)
 	if (!certificate_quality_check(ctx, &errstr))
 	{
 		unreal_log(ULOG_ERROR, "config", "TLS_CERTIFICATE_CHECK_FAILED", NULL,
-		           "There is a problem with your TLS certificate '$filename': $quality_check_error\n"
+		           "There is a problem with your TLS certificate: $quality_check_error\n"
 		           "If you use the standard UnrealIRCd certificates then you can simply run 'make pem' and 'make install' "
 		           "from your UnrealIRCd source directory (eg: ~/unrealircd-6.X.Y/) to create and install new certificates",
-		           log_data_string("filename", tlsoptions->certificate_file),
 		           log_data_string("quality_check_error", errstr));
 		goto fail;
 	}
@@ -1607,11 +1631,14 @@ void check_certificate_expiry_tlsoptions_and_warn(TLSOptions *tlsoptions)
 	if (!ctx)
 		return;
 
+	// FIXME: for dual cert the code in check_certificate_expiry_ctx()
+	//        checks only one of them, and this error message only
+	//        prints the first filename (which may not be the correct one)
 	if (check_certificate_expiry_ctx(ctx, &errstr))
 	{
 		unreal_log(ULOG_WARNING, "tls", "TLS_CERT_EXPIRING", NULL,
 		           "Warning: TLS certificate '$filename': $error_string",
-		           log_data_string("filename", tlsoptions->certificate_file),
+		           log_data_string("filename", tlsoptions->certificate_files->name),
 		           log_data_string("error_string", errstr));
 	}
 	SSL_CTX_free(ctx);

commit fd52b71081f99619173b390838ad405302e896be

Setting set::tls::certificate and set::tls::key did not override the default certificate or key. It added the cert/key to the list of certs, like a "dual cert" approach.
This was caused by commit 877d151da41f3a20fa277f0a919c69576a4611ac,
which indeed adds support for "dual cert" (or more).

I have now deferred setting the default to happen only if no
set::tls::certificate is specified, as you would expect.

We (already) used a similar delayed-initialization / deferred setting
approach in the ::tls-options inheritance code (for blocks like
listen, sni, link, etc.)

Just as a slightly related reminder, we do normally suggest keeping the
conf/tls/server.cert.pem and conf/tls/server.key.pem for server linking
and then use a cert from a trusted CA in the listen block for 6697 etc.
See https://www.unrealircd.org/docs/Using_Let's_Encrypt_with_UnrealIRCd
for more information (and the 'why').

src/conf.c
@@ -1795,10 +1795,6 @@ void config_setdefaultsettings(Configuration *i)
 
 	/* TLS options */
 	i->tls_options = safe_alloc(sizeof(TLSOptions));
-	snprintf(tmp, sizeof(tmp), "%s/tls/server.cert.pem", CONFDIR);
-	add_name_list(i->tls_options->certificate_files, tmp);
-	snprintf(tmp, sizeof(tmp), "%s/tls/server.key.pem", CONFDIR);
-	add_name_list(i->tls_options->key_files, tmp);
 	snprintf(tmp, sizeof(tmp), "%s/tls/curl-ca-bundle.crt", CONFDIR);
 	safe_strdup(i->tls_options->trusted_ca_file, tmp);
 	safe_strdup(i->tls_options->ciphers, UNREALIRCD_DEFAULT_CIPHERS);
@@ -3113,6 +3109,30 @@ int config_run_blocks(void)
 					if (cc->conffunc(cfptr, ce) < 0)
 						errors++;
 				}
+			}
+		}
+		if (!strcmp(config_block, "set"))
+		{
+			/* Yeah, this is stupid to have here, it's basically a
+			 * just-in-time placement of set::tls::certificate / key.
+			 * We can't initialize it in config_setdefaultsettings()
+			 * because when a config item is encountered in the config file
+			 * we _add to a list_ of certificates/keys, so then we
+			 * could never override the default one.
+			 * So, instead, it needs to be NULL / empty, and we only
+			 * fill it here if it is still NULL.
+			 * And we need to do it HERE, directly after processing
+			 * of all set blocks, because listen/link/sni/etc (can)
+			 * inherit set::tls options, and they should not inherit NULL.
+			 * *sigh*... -- Syzop / 2025-12-10
+			 */
+			if (!tempiConf.tls_options->certificate_files)
+			{
+				char tmp[512];
+				snprintf(tmp, sizeof(tmp), "%s/tls/server.cert.pem", CONFDIR);
+				add_name_list(tempiConf.tls_options->certificate_files, tmp);
+				snprintf(tmp, sizeof(tmp), "%s/tls/server.key.pem", CONFDIR);
+				add_name_list(tempiConf.tls_options->key_files, tmp);
 			}
 		}
 	}