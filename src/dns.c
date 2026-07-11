0bc455ae5e2f7a0407c83d62327d5db6d1ab8344
Bram Matthys <syzop@vulnscan.org>
2026-07-11 13:43:37 +0200

    Fix crash on race with 'REHASH -dns' and outgoing server linking.
    Fortunately the command 'REHASH -dns' is rarely used, but if it
    happens to be done at the same time as an outgoing linking attempt
    (well, IPv6 name resolution), then without this patch we would be
    doing a new IPv4 DNS request, while we should be shutting down the
    resolver instead.

--- src/dns.c
+++ src/dns.c
@@ -498,6 +498,14 @@ void unrealdns_cb_nametoip_link(void *arg, int status, int timeouts, struct host
 		return;
 	}
 
+	if (status == ARES_EDESTRUCTION)
+	{
+		/* Resolver is shutting down ('REHASH -dns'). We must end this request. */
+		r->linkblock->refcount--;
+		unrealdns_freeandremovereq(r);
+		return;
+	}
+
 	if ((status != 0) || !he->h_addr_list || !he->h_addr_list[0])
 	{
 		if (r->ipv6)
